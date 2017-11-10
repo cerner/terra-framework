5ffdc7912fc915a163278096d251b3832f2b60cc
Biethman,Tyler <Tyler.Biethman@Cerner.com>
2017-11-10 10:05:15 -0600

    Bunch of rem unit conversions.

--- packages/terra-framework-site/src/app/ApplicationMenu.jsx
+++ packages/terra-framework-site/src/app/ApplicationMenu.jsx
@@ -10,32 +10,17 @@ const propTypes = {
 };
 
 const ApplicationMenu = ({ routingManager }) => (
-  <ContentContainer
-    fill
-    header={(
-      <div style={{ height: '51px', display: 'flex', alignItems: 'stretch', background: '#E8E9EA' }}>
-        <div style={{ width: '60px', height: '100%', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-          <Image variant="rounded" src="https://github.com/cerner/terra-core/raw/master/terra.png" height="28px" width="28px" isFluid />
-        </div>
-        <div style={{ fontSize: '1.3rem', flex: '1 1 auto', height: '100%', display: 'flex', flexDirection: 'column', alignItems: 'start', justifyContent: 'center' }}>
-          <div style={{ paddingLeft: '10px' }}>
-            <strong>{'Terra'}</strong>{'Framework'}
-          </div>
-        </div>
-      </div>
-    )}
-  >
-    <MenuList
-      headerText="Terra Framework"
-      routingManager={routingManager}
-      links={[
-        { id: '/home', path: '/home', text: 'Home' },
-        { id: '/components', path: '/components', text: 'Components' },
-        { id: '/tests', path: '/tests', text: 'Tests' },
-      ]}
-    />
-  </ContentContainer>
+  <MenuList
+    headerText="Terra Framework"
+    routingManager={routingManager}
+    links={[
+      { id: '/home', path: '/home', text: 'Home' },
+      { id: '/components', path: '/components', text: 'Components' },
+      { id: '/tests', path: '/tests', text: 'Tests' },
+    ]}
+  />
 );
+
 ApplicationMenu.propTypes = propTypes;
 
 export default withRouter(ApplicationMenu);
