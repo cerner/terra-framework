948674faaf876aa01780b63b1da5e25ebf865ddf
Kasper,Dave <Dave.Kasper@Cerner.com>
2017-11-21 14:20:34 -0600

    Routing stack create test.

--- packages/terra-navigation-layout/tests/jest/routing/RoutingStackDelegate.test.jsx
+++ packages/terra-navigation-layout/tests/jest/routing/RoutingStackDelegate.test.jsx
@@ -3,8 +3,21 @@ import RoutingStackDelegate from '../../../src/routing/RoutingStackDelegate';
 describe('RoutingStackDelegate', () => {
   describe('create', () => {
     it('should create a RoutingStackDelegate', () => {
-      const result = RoutingStackDelegate.create({});
-      expect(result).not.toBe(undefined);
+      const testData = {
+        location: 'test location',
+        stackLocation: 'test stackLocation',
+        parentPaths: 'test parentPaths',
+        goBack: 'test goBack',
+        goToRoot: 'test goToRoot',
+        invalidKey: 'test invalidKey',
+      };
+      const result = RoutingStackDelegate.create(testData);
+      expect(result.location).toBe(testData.location);
+      expect(result.stackLocation).toBe(testData.stackLocation);
+      expect(result.parentPaths).toBe(testData.parentPaths);
+      expect(result.goBack).toBe(testData.goBack);
+      expect(result.goToRoot).toBe(testData.goToRoot);
+      expect(result.invalidKey).toBe(undefined);
     });
   });
 });
