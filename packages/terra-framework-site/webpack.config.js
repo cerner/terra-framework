a4c29e9dd19a21d881140371bff96d98b20c9d4f
Biethman,Tyler <Tyler.Biethman@Cerner.com>
2017-12-14 14:12:16 -0600

    Apparently that port won’t work

--- packages/terra-framework-site/webpack.config.js
+++ packages/terra-framework-site/webpack.config.js
@@ -93,7 +93,7 @@ module.exports = {
   devtool: 'cheap-source-map',
   devServer: {
     host: '0.0.0.0',
-    port: '0666',
+    port: '1666',
     disableHostCheck: true,
     stats: {
       assets: true,
