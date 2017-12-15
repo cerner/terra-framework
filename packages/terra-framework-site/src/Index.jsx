aec177848c2da5d77bf766b531e23f64a7748cef
Biethman,Tyler <Tyler.Biethman@Cerner.com>
2017-12-15 00:47:36 -0600

    Getting xfc auth working.

--- packages/terra-framework-site/src/Index.jsx
+++ packages/terra-framework-site/src/Index.jsx
@@ -3,6 +3,12 @@ import ReactDOM from 'react-dom';
 import { HashRouter as Router } from 'react-router-dom';
 import App from './app/App';
 import { routes, navigation } from './appConfig';
+import { Provider } from 'xfc';
+
+Provider.init({
+  acls: ['*'],
+  secret: () => (Promise.resolve('Success')),
+});
 
 ReactDOM.render((
   <Router>
