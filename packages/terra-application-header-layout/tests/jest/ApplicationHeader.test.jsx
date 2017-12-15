10f34bab54423a8348d63ab3093f4e1abfc96c70
Kasper,Dave <Dave.Kasper@Cerner.com>
2017-12-15 11:44:47 -0600

    Add combination of props for testing.

--- packages/terra-application-header-layout/tests/jest/ApplicationHeader.test.jsx
+++ packages/terra-application-header-layout/tests/jest/ApplicationHeader.test.jsx
@@ -8,7 +8,53 @@ describe('ApplicationHeaderLayout', () => {
     expect(result).toMatchSnapshot();
   });
 
-  it('should render with provided props', () => {
+  it('should render with provided extensions', () => {
+    const result = shallow(
+      <ApplicationHeaderLayout
+        extensions={<div id="test-extensions">Extensions</div>}
+      />,
+    );
+
+    expect(result).toMatchSnapshot();
+  });
+
+  it('should render with provided extensions and navigation', () => {
+    const result = shallow(
+      <ApplicationHeaderLayout
+        extensions={<div id="test-extensions">Extensions</div>}
+        navigation={<div id="test-navigation">Navigation</div>}
+      />,
+    );
+
+    expect(result).toMatchSnapshot();
+  });
+
+  it('should render with provided extensions, navigation, and logo', () => {
+    const result = shallow(
+      <ApplicationHeaderLayout
+        extensions={<div id="test-extensions">Extensions</div>}
+        navigation={<div id="test-navigation">Navigation</div>}
+        logo={<div id="test-logo">Logo</div>}
+      />,
+    );
+
+    expect(result).toMatchSnapshot();
+  });
+
+  it('should render with provided extensions, navigation, logo, and toggle', () => {
+    const result = shallow(
+      <ApplicationHeaderLayout
+        extensions={<div id="test-extensions">Extensions</div>}
+        navigation={<div id="test-navigation">Navigation</div>}
+        logo={<div id="test-logo">Logo</div>}
+        toggle={<div id="test-toggle">Toggle</div>}
+      />,
+    );
+
+    expect(result).toMatchSnapshot();
+  });
+
+  it('should render with provided extensions, navigation, logo, toggle, and utilities', () => {
     const result = shallow(
       <ApplicationHeaderLayout
         extensions={<div id="test-extensions">Extensions</div>}
@@ -16,6 +62,82 @@ describe('ApplicationHeaderLayout', () => {
         logo={<div id="test-logo">Logo</div>}
         toggle={<div id="test-toggle">Toggle</div>}
         utilities={<div id="test-utilities">Utilities</div>}
+      />,
+    );
+
+    expect(result).toMatchSnapshot();
+  });
+
+  it('should render with provided navigation, logo, toggle, and utilities', () => {
+    const result = shallow(
+      <ApplicationHeaderLayout
+        navigation={<div id="test-navigation">Navigation</div>}
+        logo={<div id="test-logo">Logo</div>}
+        toggle={<div id="test-toggle">Toggle</div>}
+        utilities={<div id="test-utilities">Utilities</div>}
+      />,
+    );
+
+    expect(result).toMatchSnapshot();
+  });
+
+  it('should render with provided logo, toggle, and utilities', () => {
+    const result = shallow(
+      <ApplicationHeaderLayout
+        logo={<div id="test-logo">Logo</div>}
+        toggle={<div id="test-toggle">Toggle</div>}
+        utilities={<div id="test-utilities">Utilities</div>}
+      />,
+    );
+
+    expect(result).toMatchSnapshot();
+  });
+
+  it('should render with provided toggle and utilities', () => {
+    const result = shallow(
+      <ApplicationHeaderLayout
+        toggle={<div id="test-toggle">Toggle</div>}
+        utilities={<div id="test-utilities">Utilities</div>}
+      />,
+    );
+
+    expect(result).toMatchSnapshot();
+  });
+
+  it('should render with provided utilities', () => {
+    const result = shallow(
+      <ApplicationHeaderLayout
+        utilities={<div id="test-utilities">Utilities</div>}
+      />,
+    );
+
+    expect(result).toMatchSnapshot();
+  });
+
+  it('should render with provided navigation', () => {
+    const result = shallow(
+      <ApplicationHeaderLayout
+        navigation={<div id="test-navigation">Navigation</div>}
+      />,
+    );
+
+    expect(result).toMatchSnapshot();
+  });
+
+  it('should render with provided logo', () => {
+    const result = shallow(
+      <ApplicationHeaderLayout
+        logo={<div id="test-logo">Logo</div>}
+      />,
+    );
+
+    expect(result).toMatchSnapshot();
+  });
+
+  it('should render with provided toggle', () => {
+    const result = shallow(
+      <ApplicationHeaderLayout
+        toggle={<div id="test-toggle">Toggle</div>}
       />,
     );
 
