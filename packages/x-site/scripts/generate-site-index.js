a3f2044f1a8b3e838fdb41ebea3737a86905dfa4
Biethman,Tyler <Tyler.Biethman@Cerner.com>
2017-12-21 12:11:51 -0600

    Moving scripts over.

--- packages/x-site/scripts/generate-site-index.js
+++ packages/x-site/scripts/generate-site-index.js
@@ -5,35 +5,69 @@ const kebabCase = require('lodash.kebabcase');
 const startCase = require('lodash.startcase');
 
 const repositoryName = JSON.parse(fs.readFileSync(path.resolve(__dirname, '..', '..', 'package.json'))).name;
-
 const packagePath = path.join(__dirname, '..', '..', 'packages');
 
-glob(path.join(packagePath, '**', '*.example?(.jsx|.js)'), (er, files) => {
+glob(path.join(packagePath, '*', 'lib', 'examples', '*.example?(.jsx|.js)'), (er, files) => {
   const parsedFiles = {};
   parsedFiles[repositoryName] = {
     name: startCase(repositoryName),
     packages: [],
   };
 
+  console.log(files);
+
   files.forEach((filePath) => {
     const parsedPath = path.parse(filePath);
     const parsedDirectory = parsedPath.dir.split(path.sep);
 
     const fileName = parsedPath.name.replace('.example', '');
-    const packageName = parsedDirectory[parsedDirectory.length - 2];
+    const packageName = parsedDirectory[parsedDirectory.length - 3];
 
     parsedFiles[repositoryName].packages.push({
       name: fileName,
-      url: `${process.argv.length > 2 ? process.argv[2] : 'http://engineering.cerner.com'}/${repositoryName}/examples/${kebabCase(packageName)}/${kebabCase(fileName)}`,
+      packageName,
+      path: filePath,
+      url: `${process.argv.length > 2 ? process.argv[2] : 'http://engineering.cerner.com'}/${repositoryName}/raw/examples/${kebabCase(packageName)}/${kebabCase(fileName)}`,
     });
   });
 
   // Write JSON file for terra-ui consumption
-  fs.writeFileSync(path.join(__dirname, '..', '..', 'site-examples.json'), JSON.stringify(parsedFiles, null, '  '));
+  if (!fs.existsSync(path.join(__dirname, '..', '..', 'terra-ui-config'))) {
+    fs.mkdir(path.join(__dirname, '..', '..', 'terra-ui-config'), () => {
+      fs.writeFileSync(path.join(__dirname, '..', '..', 'terra-ui-config', 'configuration.json'), JSON.stringify(parsedFiles, null, '  '));
+    });
+  } else {
+    fs.writeFileSync(path.join(__dirname, '..', '..', 'terra-ui-config', 'configuration.json'), JSON.stringify(parsedFiles, null, '  '));
+  }
+
+  console.log(parsedFiles);
 
   // Generate configuration file for site consumption
-  fs.readFile(path.join(__dirname, 'indexTemplate.js'), 'utf8', (err, data) => {
-    const newFile = data.concat('\n', `const output = ${JSON.stringify(parsedFiles, null, '  ')};`);
-    fs.writeFileSync(path.join(__dirname, '..', '..', 'generatedIndex.js'), newFile);
+  fs.readFile(path.join(__dirname, 'index.template'), 'utf8', (err, data) => {
+    let newFileContent = data.replace('<imports>', parsedFiles[repositoryName].packages.map((package) => {
+      return `import ${package.name} from '${package.packageName}/lib/examples/${package.name}.example';`;
+    }).join('\n'));
+
+    let configString = `${parsedFiles[repositoryName].packages.map((package) => (
+      `'${package.packageName}': {
+        name: '${package.name}',
+        example: {
+          path: '/examples/${kebabCase(package.name)}',
+          component: ${package.name},
+          description: '${package.name}',
+        },
+      },`
+    )).join(`\n`)}`;
+
+    console.log(configString);
+
+    newFileContent = newFileContent.replace('<config>',
+      `const config = {
+        ${configString}
+      };`
+    );
+
+    // const newFile = data.concat('\n', `const output = ${JSON.stringify(parsedFiles, null, '  ')};`);
+    fs.writeFileSync(path.join(__dirname, '..', '..', 'generatedIndex.js'), newFileContent);
   });
 });
