2c8f5d8df613be79aae97f982df4c94ba04dc11e
Biethman,Tyler <Tyler.Biethman@Cerner.com>
2018-01-28 15:59:27 -0600

    Updates for framework

--- packages/terra-aggregator/tests/jest/Aggregator.test.jsx
+++ packages/terra-aggregator/tests/jest/Aggregator.test.jsx
@@ -9,16 +9,4 @@ describe('Aggregator', () => {
     const wrapper = shallow(defaultRender);
     expect(wrapper).toMatchSnapshot();
   });
-
-  // Prop Tests
-  it('should use the default value when no value is given', () => {
-    const wrapper = shallow(defaultRender);
-    expect(wrapper.find('.aggregator').text()).toEqual('defualt');
-  });
-
-  // Structure Tests
-  it('should have the class aggregator', () => {
-    const wrapper = shallow(defaultRender);
-    expect(wrapper.prop('className')).toContain('aggregator');
-  });
 });
