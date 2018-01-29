e4c3b46d1c09b480d5d24d04ec99ac1831c8769d
Rohrbough,Emily <Emily.Rohrbough@cerner.com>
2018-01-29 16:01:53 -0600

    Changed generatedConfig ref

--- site.config.js
+++ site.config.js
@@ -4,7 +4,7 @@ const homeReadMe = fs.readFileSync('./README.md', 'utf-8');
 
 module.exports = {
   /* The component configuration path. */
-  componentConfigPath: './site/generatedComponentConfig.js',
+  componentConfigPath: './generatedComponentConfig.js',
 
   /* The logo to display as the placeholder. */
   placeholderSrc: 'https://github.com/cerner/terra-core/raw/master/terra.png',
