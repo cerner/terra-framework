8341499bf2fcc798cc8cd697e3f9fc2c7ef27150
Kasper,Dave <Dave.Kasper@Cerner.com>
2018-02-05 13:34:10 -0600

    Add wdio functionality.

---
+++ wdio.conf.js
@@ -0,0 +1,43 @@
+/* eslint-disable import/no-extraneous-dependencies */
+const wdioConf = require('terra-toolkit/lib/wdio/conf');
+const WebpackDevService = require('terra-toolkit/lib/wdio/services/index').WebpackDevService;
+const localIP = require('ip');
+const path = require('path');
+const webpackConfig = require('./node_modules/terra-site/src/config/webpack.config.js');
+
+const webpackPort = 8080;
+
+// Flex specs search between local pacakge and repo
+let specs = path.join('tests', 'wdio', '**', '*-spec.js');
+if (__dirname === process.cwd()) {
+  specs = path.join('packages', '*', specs);
+}
+
+const config = {
+  ...wdioConf.config,
+
+  baseUrl: `http://${localIP.address()}:${webpackPort}`,
+  specs,
+
+  // Travis only has 1 browser instace, set maxInstances to 1 to prevent timeouts
+  maxInstances: process.env.CI ? 1 : wdioConf.config.maxInstances,
+  seleniumDocker: {
+    enabled: !process.env.TRAVIS,
+  },
+
+  // Ignore deprecation warnings. When chrome supports /actions API we'll update to use those.
+  deprecationWarnings: false,
+
+  webpackPort,
+  webpackConfig,
+
+  beforeHook() {
+    // Being Terra tests are executed on an SPA, a full refresh is required
+    // in order to reset the site. This ensures customProperty tests and any
+    // other dom modifications are cleared before starting a test.
+    global.browser.refresh();
+  },
+};
+
+config.services = wdioConf.config.services.concat([WebpackDevService]);
+exports.config = config;
