c99897809be2b2d8a8df5ebe415b2c3e419dfb3f
Tyler Biethman <tbiethman@users.noreply.github.com>
2018-02-09 09:44:26 -0600

    Creating SlidePanelManager (#60)
    * Creating terra-slide-panel-manager package.
    
    * Updating docs.
    
    * Adding example files.
    
    * Adding example files.
    
    * Removing old package refs
    
    * Getting inline styles out of examples
    
    * Adding wdio tests
    
    * PR updates.
    
    * Updating link ref
    
    * Removing themeable-variables file
    
    * Using default example/test example from DisclosureManager.

---
+++ packages/terra-slide-panel-manager/README.md
@@ -0,0 +1,24 @@
+# Terra SlidePanel Manager
+
+[![NPM version](http://img.shields.io/npm/v/terra-slide-panel-manager.svg)](https://www.npmjs.org/package/terra-slide-panel-manager)
+[![Build Status](https://travis-ci.org/cerner/terra-framework.svg?branch=master)](https://travis-ci.org/cerner/terra-framework)
+
+The SlidePanelManager is a DisclosureManager implementation that presents disclosed content using a SlidePanel.
+
+- [Getting Started](#getting-started)
+- [Documentation](https://github.com/cerner/terra-framework/tree/master/packages/terra-slide-panel-manager/docs)
+- [LICENSE](#license)
+
+## Getting Started
+
+- Install from [npmjs](https://www.npmjs.com): `npm install terra-slide-panel-manager`
+
+## LICENSE
+
+Copyright 2017 Cerner Innovation, Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at
+
+&nbsp;&nbsp;&nbsp;&nbsp;http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
---
+++ packages/terra-slide-panel-manager/src/SlidePanelManager.jsx
@@ -0,0 +1,93 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import AppDelegate from 'terra-app-delegate';
+import DisclosureManager, { availableDisclosureSizes } from 'terra-disclosure-manager';
+import SlideGroup from 'terra-slide-group';
+import SlidePanel from 'terra-slide-panel';
+
+const disclosureType = 'panel';
+export { disclosureType };
+
+const propTypes = {
+  /**
+   * An AppDelegate instance that will be used in fallback scenarios by the SlidePanelManager. If not provided, the SlidePanelManager
+   * will handle any and all disclosure requests it receives.
+   */
+  app: AppDelegate.propType,
+  /**
+   * The components to display in the body of the SlidePanel. These components will be provided with an AppDelegate prop (as `app`) that
+   * will allow them to disclose content.
+   */
+  children: PropTypes.node,
+  /**
+   * The desired panel behavior. Either 'squish' or 'overlay'.
+   */
+  panelBehavior: PropTypes.oneOf(['overlay', 'squish']),
+};
+
+const defaultProps = {
+  panelBehavior: 'overlay',
+};
+
+/**
+ * The SlidePanel component does not support the full range of DisclosureManager sizes in its API. The potential sizes are mapped to the
+ * SlidePanel's supported sizes.
+ */
+const disclosureSizeToPanelSize = {
+  [availableDisclosureSizes.TINY]: 'small',
+  [availableDisclosureSizes.SMALL]: 'small',
+  [availableDisclosureSizes.MEDIUM]: 'large',
+  [availableDisclosureSizes.LARGE]: 'large',
+  [availableDisclosureSizes.HUGE]: 'large',
+};
+
+class SlidePanelManager extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.renderSlidePanel = this.renderSlidePanel.bind(this);
+  }
+
+  renderSlidePanel(manager) {
+    const { app, children, panelBehavior, ...customProps } = this.props;
+
+    let isFullscreen;
+    if (manager.disclosure.size === availableDisclosureSizes.FULLSCREEN || manager.disclosure.isMaximized) {
+      isFullscreen = true;
+    }
+
+    return (
+      <SlidePanel
+        {...customProps}
+        fill
+        panelBehavior={panelBehavior}
+        isFullscreen={isFullscreen}
+        panelSize={disclosureSizeToPanelSize[manager.disclosure.size]}
+        isOpen={manager.disclosure.isOpen}
+        panelContent={(
+          <SlideGroup items={manager.disclosure.components} isAnimated />
+        )}
+        mainContent={manager.children.components}
+      />
+    );
+  }
+
+  render() {
+    const { app, children } = this.props;
+
+    return (
+      <DisclosureManager
+        app={app}
+        supportedDisclosureTypes={[disclosureType]}
+        render={this.renderSlidePanel}
+      >
+        {children}
+      </DisclosureManager>
+    );
+  }
+}
+
+SlidePanelManager.propTypes = propTypes;
+SlidePanelManager.defaultProps = defaultProps;
+
+export default SlidePanelManager;
