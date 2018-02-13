33d7e4897989121e27a707927361951c429bdbde
Kasper,Dave <Dave.Kasper@Cerner.com>
2018-02-13 14:30:53 -0600

    Updated wdio tests.

--- packages/terra-navigation-side-menu/examples/test-examples/NavigationSideMenuDefault.jsx
+++ packages/terra-navigation-side-menu/examples/test-examples/NavigationSideMenuDefault.jsx
@@ -8,15 +8,18 @@ const NavigationSideMenuDefault = () => (
       <NavigationSideMenu
         id="test-menu"
         menuItems={[
-          { key: 'test1', text: 'Test Menu 1', id: 'test-item-1' },
-          { key: 'test2', text: 'Test Menu 2', id: 'test-item-2' },
-          { key: 'test3', text: 'Test Menu 3', id: 'test-item-3' },
-          { key: 'test4', text: 'Test Menu 4', id: 'test-item-4' },
+          { key: 'menu', title: 'Menu', children: ['submenu1', 'submenu2', 'submenu3', 'submenu4'] },
+          { key: 'submenu1', title: 'Sub Menu 1', children: ['subsubmenu1', 'subsubmenu2', 'subsubmenu3'], id: 'test-item-1' },
+          { key: 'submenu2', title: 'Sub Menu 2' },
+          { key: 'submenu3', title: 'Sub Menu 3' },
+          { key: 'submenu4', title: 'Sub Menu 4' },
+          { key: 'subsubmenu1', title: 'Sub-Sub Menu 1', id: 'test-item-2' },
+          { key: 'subsubmenu2', title: 'Sub-Sub Menu 2' },
+          { key: 'subsubmenu3', title: 'Sub-Sub Menu 3' },
         ]}
         onChange={() => {}}
         routingStackDelegate={{ showParent: () => {} }}
-        selectedKey="test1"
-        title="Test Tile"
+        initialSelectedKey="menu"
       />
     </div>
   </Base>
--- packages/terra-navigation-side-menu/tests/wdio/navigation-side-menu-spec.js
+++ packages/terra-navigation-side-menu/tests/wdio/navigation-side-menu-spec.js
@@ -1,11 +1,11 @@
 /* global browser, Terra, before */
 
-describe('Large size squished slide panel', () => {
+describe('Default navigation side menu display', () => {
   before(() => browser.setViewportSize(Terra.viewports('medium')[0]));
   beforeEach(() => {
     browser.url('/#/raw/tests/navigation-side-menu/navigation-side-menu-default');
     browser.waitForVisible('#test-menu');
-    browser.moveToObject('#test-menu #test-item-3');
+    browser.moveToObject('#test-menu #test-item-1');
   });
 
   Terra.should.matchScreenshot('#test-menu', { selector: '#test-menu' });
@@ -17,6 +17,22 @@ describe('Large size squished slide panel', () => {
     '--terra-navigation-side-menu-item-padding-left': '2rem',
     '--terra-navigation-side-menu-item-padding-right': '4rem',
     '--terra-navigation-side-menu-item-background-hover': 'linear-gradient(-90deg, pink, blue)',
+  });
+});
+
+describe('Selected navigation side menu display', () => {
+  before(() => browser.setViewportSize(Terra.viewports('medium')[0]));
+  beforeEach(() => {
+    browser.url('/#/raw/tests/navigation-side-menu/navigation-side-menu-default');
+    browser.waitForVisible('#test-menu');
+    browser.moveToObject('#test-menu #test-item-1');
+    browser.click('#test-menu #test-item-1');
+    browser.click('#test-menu #test-item-2');
+  });
+
+  Terra.should.matchScreenshot('#test-menu', { selector: '#test-menu' });
+  Terra.should.beAccessible({ context: '#test-menu' });
+  Terra.should.themeEachCustomProperty('#test-menu', {
     '--terra-navigation-side-menu-item-selected-background-image': 'linear-gradient(-270deg, pink, blue)',
     '--terra-navigation-side-menu-item-selected-border-left': '1rem dotted orange',
   });
