649eb7faa12f229c3cb31b2bc57cca04aa301619
Dave Kasper <dave.m.kasper@gmail.com>
2018-02-14 20:57:29 -0600

    Provide an index file for the json main defaulted entry. (#75)
    * Provide an index file for the json main defaulted entry.
    
    * Update change log.
    
    * Change header default styling to work with background color of header.

--- packages/terra-application-name/examples/test-examples/ApplicationHeaderNameDefault.jsx
+++ packages/terra-application-name/examples/test-examples/ApplicationHeaderNameDefault.jsx
@@ -3,9 +3,11 @@ import Image from 'terra-image';
 import ApplicationHeaderName from '../../lib/ApplicationHeaderName';
 
 export default () => (
-  <ApplicationHeaderName
-    id="default"
-    title="Title"
-    accessory={<Image alt="terra accessory" variant="rounded" src="https://github.com/cerner/terra-core/raw/master/terra.png" height="26px" width="26px" />}
-  />
+  <div style={{ backgroundColor: 'green' }}>
+    <ApplicationHeaderName
+      id="default"
+      title="Title"
+      accessory={<Image alt="terra accessory" variant="rounded" src="https://github.com/cerner/terra-core/raw/master/terra.png" height="26px" width="26px" />}
+    />
+  </div>
 );
---
+++ packages/terra-application-name/src/ApplicationName.js
@@ -0,0 +1,10 @@
+import ApplicationHeaderName from './ApplicationHeaderName';
+import ApplicationMenuName from './ApplicationMenuName';
+
+const ApplicationName = {
+  ApplicationHeaderName,
+  ApplicationMenuName,
+};
+
+export default ApplicationName;
+export { ApplicationHeaderName, ApplicationMenuName };
