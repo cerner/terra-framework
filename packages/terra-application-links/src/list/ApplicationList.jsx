8e5c1a777cb5e204965a590f6d117170a65fcbf4
Kasper,Dave <Dave.Kasper@Cerner.com>
2018-02-19 13:54:43 -0600

    Update prop name.

--- packages/terra-application-links/src/list/ApplicationList.jsx
+++ packages/terra-application-links/src/list/ApplicationList.jsx
@@ -14,7 +14,7 @@ const propTypes = {
    * Navigational links that will generate list items that will update the path. These paths are matched with react-router to selection.
    */
   links: PropTypes.arrayOf(PropTypes.shape({
-    hasChevron: PropTypes.bool,
+    hasSubMenu: PropTypes.bool,
     id: PropTypes.string,
     path: PropTypes.string.isRequired,
     text: PropTypes.string.isRequired,
@@ -43,7 +43,7 @@ const ApplicationList = ({
           <div className={cx('title')}>
             {link.text}
           </div>
-          {link.hasChevron && <span className={cx('chevron')}><ChevronRight height="0.8em" width="0.8em" /></span>}
+          {link.hasSubMenu && <span className={cx('chevron')}><ChevronRight height="0.8em" width="0.8em" /></span>}
         </NavLink>
       }
       key={link.path}
