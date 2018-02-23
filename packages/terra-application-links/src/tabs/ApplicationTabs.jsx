76d6791a77908a20ccdffd53c5469b712d76bf1f
Kasper, Dave <dave.m.kasper@gmail.com>
2018-02-23 15:41:24 -0600

    correct broken prototype.

--- packages/terra-application-links/src/tabs/ApplicationTabs.jsx
+++ packages/terra-application-links/src/tabs/ApplicationTabs.jsx
@@ -16,7 +16,20 @@ const propTypes = {
   /**
    * Navigational links that will generate tabs that will update the path. These paths are matched with react-router to selection.
    */
-  links: TabUtils.ApplicationLinksPropType,
+  links: PropTypes.arrayOf(PropTypes.shape({
+    /**
+     * The id to append to the link.
+     */
+    id: PropTypes.string,
+    /**
+     * The path to push to the route.
+     */
+    path: PropTypes.string.isRequired,
+    /**
+     * The display text for the link.
+     */
+    text: PropTypes.string.isRequired,
+  })),
   /**
    * The location as provided by the `withRouter()` HOC.
    */
