fcd334e00854d3d7b4649298c0ab7215204f5104
Dave Kasper <dave.m.kasper@gmail.com>
2018-02-27 13:57:46 -0600

    Remove hover style at medium breakpoint. (#80)
    * Remove hover style at medium breakpoint.
    
    * Add change log.
    
    * Remove heights on tests.
    
    * Revert "Remove heights on tests."
    
    This reverts commit de2f15ef05dd6e4668612b4140c63456f5230b4e.
    
    * Update setAttribute to use string.
    
    * Update documentation.

--- generatedComponentConfig.js
+++ generatedComponentConfig.js
@@ -30,7 +30,6 @@ import ApplicationMenuNameTruncated from './packages/terra-application-name/exam
 import DefaultApplicationUtility from './packages/terra-application-utility/examples/test-examples/DefaultApplicationUtility';
 import DefaultEmbeddedComponent from './packages/terra-embedded-component/examples/test-examples/DefaultEmbeddedComponent';
 import LayoutDefault from './packages/terra-layout/examples/test-examples/LayoutDefault';
-import LayoutLongText from './packages/terra-layout/examples/test-examples/LayoutLongText';
 import LayoutNoHeader from './packages/terra-layout/examples/test-examples/LayoutNoHeader';
 import LayoutNoMenu from './packages/terra-layout/examples/test-examples/LayoutNoMenu';
 import LayoutSlidePanelExample from './packages/terra-layout/examples/test-examples/LayoutSlidePanelExample';
@@ -228,11 +227,6 @@ const componentConfig = {
         name: 'Layout Default',
         path: '/layout-default',
         component: LayoutDefault,
-      },
-      {
-        name: 'Layout Long Text',
-        path: '/layout-long-text',
-        component: LayoutLongText,
       },
       {
         name: 'Layout No Header',
--- packages/terra-layout/examples/test-examples/LayoutNoMenu.jsx
+++ packages/terra-layout/examples/test-examples/LayoutNoMenu.jsx
@@ -5,6 +5,7 @@ import TestContent from './test-content/TestContent';
 
 const LayoutNoMenu = () => (
   <Layout
+    style={{ height: '768px' }}
     key="layout-no-menu"
     header={<TestHeader />}
     id="test-root"
