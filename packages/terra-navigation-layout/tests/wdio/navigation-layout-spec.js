05b7cbb57f73c13d9178cbb2d0163e738c580794
Sairam Rakshith Bhyravabhotla <rakshith.bhyravabhotla@gmail.com>
2018-03-06 09:09:32 -0600

    Add webdriver.io tests fro terra-navigation-layout and remove nightwatch dependency. (#94)
    * Remove obselete nightwatch tests
    
    * wdio tests for terra-navigation-layout
    
    * Remove nightwatch dependency
    
    * Upload snapshots

---
+++ packages/terra-navigation-layout/tests/wdio/navigation-layout-spec.js
@@ -0,0 +1,41 @@
+/* global browser, Terra, before */
+
+const viewports = Terra.viewports('tiny', 'small', 'medium', 'large', 'huge');
+const selector = '#test-root';
+
+describe('Navigation Layout', () => {
+  describe('Displays a basic NavigationLayout', () => {
+    before(() => {
+      browser.url('/#/raw/tests/navigation-layout/navigation-layout-basic');
+    });
+
+    Terra.should.matchScreenshot({ viewports, selector });
+  });
+
+  describe('Displays a complex NavigationLayout - page 1', () => {
+    before(() => {
+      browser.url('/#/raw/tests/navigation-layout/navigation-layout-complex');
+      browser.click('#test-root .page-1-link');
+    });
+
+    Terra.should.matchScreenshot({ viewports, selector });
+  });
+
+  describe('Displays a complex NavigationLayout - page 2', () => {
+    before(() => {
+      browser.url('/#/raw/tests/navigation-layout/navigation-layout-complex');
+      browser.click('#test-root .page-2-link');
+    });
+
+    Terra.should.matchScreenshot({ viewports, selector });
+  });
+
+  describe('Displays a complex NavigationLayout - page 3', () => {
+    before(() => {
+      browser.url('/#/raw/tests/navigation-layout/navigation-layout-complex');
+      browser.click('#test-root .page-3-link');
+    });
+
+    Terra.should.matchScreenshot({ viewports, selector });
+  });
+});
