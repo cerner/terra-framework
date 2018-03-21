1f799de002286c62b6f34268385aff6be70535a5
Kasper, Dave <dave.m.kasper@gmail.com>
2018-03-21 16:08:39 -0500

    Jest tests for header and menu.

---
+++ packages/terra-application-layout/tests/jest/ApplicationMenu.test.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+
+import ApplicationMenu from '../../src/menu/_ApplicationMenu';
+import ExtensionsExample from '../../examples/test-examples/ExtensionsExample';
+import ContentExample from '../../examples/test-examples/ContentExample';
+
+describe('ApplicationHeader', () => {
+  it('should render default component', () => {
+    const testHeader = (
+      <ApplicationMenu
+        routingStackDelegate={{}}
+        layoutConfig={{ size: 'small' }}
+      />
+    );
+
+    const result = shallow(testHeader);
+
+    expect(result).toMatchSnapshot();
+  });
+
+  it('should render props', () => {
+    const testHeader = (
+      <ApplicationMenu
+        id="test-menu"
+        layoutConfig={{ size: 'small' }}
+        nameConfig={{ title: 'app-test-title' }}
+        routingStackDelegate={{}}
+        utilityConfig={{
+          title: 'test-util-title',
+          initialSelectedKey: 'test-menu',
+          onChange: () => {},
+          menuItems: [
+            {
+              key: 'test-menu',
+              contentLocation: 'body',
+              title: 'test-menu-title',
+            },
+          ],
+        }}
+        extensions={<ExtensionsExample layoutConfig={{ size: 'small' }} />}
+        content={<ContentExample />}
+      />
+    );
+
+    const result = shallow(testHeader);
+
+    expect(result).toMatchSnapshot();
+  });
+});
