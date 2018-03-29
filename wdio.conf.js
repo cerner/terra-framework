08764f6881005e4be6bdec453b1f2fd2fe5dd0a6
Ryan Manuel <rfmanuel@gmail.com>
2018-03-29 16:32:00 -0500

    Update wdio.conf.js

--- wdio.conf.js
+++ wdio.conf.js
@@ -40,6 +40,37 @@ const config = {
   terra: {
     selector: '[data-terra-dev-site-content] *:first-child',
   },
+  
+  // Define wdio test suites
+  suites: {
+    suite1: [
+      'packages/terra-aggregator/tests/wdio/**/*-spec.js',
+      'packages/terra-app-delegate/tests/wdio/**/*-spec.js',
+      'packages/terra-application-header-layout/tests/wdio/**/*-spec.js',
+      'packages/terra-application-layout/tests/wdio/**/*-spec.js',
+      'packages/terra-application-links/tests/wdio/**/*-spec.js',
+    ],
+    suite2: [
+      'packages/terra-application-menu-layout/tests/wdio/**/*-spec.js',
+      'packages/terra-application-name/tests/wdio/**/*-spec.js',
+      'packages/terra-application-utility/tests/wdio/**/*-spec.js',
+      'packages/terra-disclosure-manager/tests/wdio/**/*-spec.js',
+      'packages/terra-hookshot/tests/wdio/**/*-spec.js',
+    ],
+    suite3: [
+      'packages/terra-infinite-list/tests/wdio/**/*-spec.js',
+      'packages/terra-layout/tests/wdio/**/*-spec.js',
+      'packages/terra-modal-manager/tests/wdio/**/*-spec.js',
+      'packages/terra-navigation-layout/tests/wdio/**/*-spec.js',
+      'packages/terra-navigation-side-menu/tests/wdio/**/*-spec.js',
+    ],
+    suite4: [
+      'packages/terra-popup/tests/wdio/**/*-spec.js',
+      'packages/terra-slide-panel-manager/tests/wdio/**/*-spec.js',
+      'packages/terra-slide-panel/tests/wdio/**/*-spec.js',
+      'packages/terra-theme-provider/tests/wdio/**/*-spec.js',
+    ],
+  },
 
   beforeHook() {
     // Being Terra tests are executed on an SPA, a full refresh is required
