17b2784299847c4d7ddb23dc19d33e1b02080ab4
Kasper, Dave <dave.m.kasper@gmail.com>
2018-04-20 12:52:32 -0500

    Adding documentation files.

---
+++ packages/terra-modal/examples/ChangeLog.site-page.jsx
@@ -0,0 +1,17 @@
+/* eslint-disable import/no-extraneous-dependencies, import/no-webpack-loader-syntax, import/first, import/no-unresolved, import/extensions */
+import React from 'react';
+import Markdown from 'terra-markdown';
+
+import ChangeLog from '../CHANGELOG.md';
+import ExampleDoc from '../docs/example.md';
+import { version } from '../package.json';
+
+const ModalExamples = () => (
+  <div>
+    <div id="version">Version: {version}</div>
+    <Markdown id="example" src={ExampleDoc} />
+    <Markdown id="change" src={ChangeLog} />
+  </div>
+);
+
+export default ModalExamples;
