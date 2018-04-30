e91c9ab20f5f84805deeebc47a8ff116c33c65a2
Sai Kaushik Mallela <Kaushik.Mallela@cerner.com>
2018-04-30 10:43:08 -0500

    updated copyright text in site example

--- packages/terra-brand-footer/examples/index-examples/DefaultBrandFooter.example.jsx
+++ packages/terra-brand-footer/examples/index-examples/DefaultBrandFooter.example.jsx
@@ -47,7 +47,7 @@ export default () =>
     }
     contentBottom={
       <span>
-        <small>Copyright &copy; 2018 Cerner Corporation.  All Rights Reserved.</small>
+        <small>Copyright 2018 Cerner Innovation, Inc.  Licensed under the Apache License, Version 2.0 (the &quot;License&quot;).</small>
       </span>
     }
   />;
