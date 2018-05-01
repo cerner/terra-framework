f7f5e803d123fe466d7c2dcd200f4437ce2c432d
Sairam Rakshith Bhyravabhotla <rakshith.bhyravabhotla@gmail.com>
2018-05-01 16:47:47 -0500

    more jest

--- packages/terra-notification-dialog/tests/jest/NotificationDialog.test.jsx
+++ packages/terra-notification-dialog/tests/jest/NotificationDialog.test.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import CompleteNotificationDialog from '../../examples/test-examples/CompleteNotificationDialog.example';
 import CompleteNotificationDialogWithLongMessage from '../../examples/test-examples/CompleteNotificationDialogWithLongMessage.example';
+import NoVariantNotificationDialog from '../../examples/test-examples/NoVariantNotificationDialog.example';
 
 it('should shallow a complete notification-dialog', () => {
   const modal = shallow(<CompleteNotificationDialog />);
   expect(modal).toMatchSnapshot();
 });
 
-it('should mount an complete notification-dialog', () => {
+it('should mount a complete notification-dialog', () => {
   const modal = mount(<CompleteNotificationDialog />);
   expect(modal).toMatchSnapshot();
 });
@@ -17,7 +18,17 @@ it('should shallow a complete notification-dialog with long message', () => {
   expect(modal).toMatchSnapshot();
 });
 
-it('should mount an complete notification-dialog with long message', () => {
+it('should mount a complete notification-dialog with long message', () => {
   const modal = mount(<CompleteNotificationDialogWithLongMessage />);
+  expect(modal).toMatchSnapshot();
+});
+
+it('should shallow a notification-dialog with no variant', () => {
+  const modal = shallow(<NoVariantNotificationDialog />);
+  expect(modal).toMatchSnapshot();
+});
+
+it('should mount a notification-dialog with no variant', () => {
+  const modal = mount(<NoVariantNotificationDialog />);
   expect(modal).toMatchSnapshot();
 });
