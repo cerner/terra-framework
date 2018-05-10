7953b3e2df77d881f747b6daaad0ada1c3b577af
Sairam Rakshith Bhyravabhotla <rakshith.bhyravabhotla@gmail.com>
2018-05-10 15:55:05 -0500

    more wdio test:

--- packages/terra-dialog-modal/tests/wdio/dialog-modal-spec.js
+++ packages/terra-dialog-modal/tests/wdio/dialog-modal-spec.js
@@ -12,6 +12,26 @@ describe('DialogModal', () => {
     Terra.should.matchScreenshot({ viewports, selector });
   });
 
+  describe('Default Dialog Modal with Themable Properties', () => {
+    beforeEach(() => {
+      browser.url('/#/raw/tests/dialog-modal/default-dialog-modal');
+      browser.click('#trigger-dialog-modal');
+    });
+
+    Terra.should.themeEachCustomProperty(
+      '#root',
+      {
+        '--terra-dialog-modal-border-radius': '1rem',
+        '--terra-dialog-modal-header-color': '#c00',
+        '--terra-dialog-modal-header-border': '1rem solid #c00',
+        '--terra-dialog-modal-body-color': '#000',
+        '--terra-dialog-modal-body-padding': '0rem',
+        '--terra-dialog-modal-footer-color': '#c00',
+        '--terra-dialog-modal-footer-border': '1rem solid #c00',
+      },
+    );
+  });
+
   describe('Dialog Modal With Long text', () => {
     beforeEach(() => {
       browser.url('/#/raw/tests/dialog-modal/dialog-modal-with-long-text');
