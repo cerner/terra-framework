306d828c19901c43b41ae7e1494c345d2184c0e6
Kasper, Dave <dave.m.kasper@gmail.com>
2018-06-02 22:59:45 -0500

    Tests.

---
+++ packages/terra-hookshot/src/terra-dev-site/test/hookshot/HookshotCoordinates.test.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import Hookshot from '../../../Hookshot';
+import HookshotContent from '../common/HookshotContentTestTemplate';
+
+class HookshotTemplate extends React.Component {
+  constructor(props) {
+    super(props);
+    this.triggerHookshot = this.triggerHookshot.bind(this);
+    this.handleRequestClose = this.handleRequestClose.bind(this);
+    this.state = { open: false };
+  }
+
+  triggerHookshot() {
+    this.setState({ open: true });
+  }
+
+  handleRequestClose() {
+    this.setState({ open: false });
+  }
+
+  render() {
+    return (
+      <div id="coords-test" style={{ border: '1px dashed grey', height: '300px', width: '300px' }}>
+        <Hookshot
+          contentAttachment={{ vertical: 'top', horizontal: 'start' }}
+          targetCoordinates={{ x: 50, y: 132 }}
+          isEnabled
+          isOpen={this.state.open}
+        >
+          <HookshotContent
+            id="test-coords-content"
+            onEsc={this.handleRequestClose}
+            onOutsideClick={this.handleRequestClose}
+          />
+        </Hookshot>
+        <button
+          id="coords-button"
+          onClick={this.triggerHookshot}
+        >
+          Trigger Hookshot
+        </button>
+      </div>
+    );
+  }
+}
+
+export default HookshotTemplate;
