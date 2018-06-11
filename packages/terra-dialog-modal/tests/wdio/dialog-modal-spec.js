d29ba11619d911263d2e6aff1791322656ebe101
Sairam Rakshith Bhyravabhotla <rakshith.bhyravabhotla@gmail.com>
2018-06-11 15:55:37 -0500

    address comments

--- packages/terra-dialog-modal/tests/wdio/dialog-modal-spec.js
+++ packages/terra-dialog-modal/tests/wdio/dialog-modal-spec.js
@@ -26,11 +26,11 @@ Terra.viewports('tiny', 'medium', 'large').forEach((viewport) => {
       selector: '#root',
       properties: {
         '--terra-dialog-modal-border-radius': '1rem',
-        '--terra-dialog-modal-header-color': '#c00',
+        '--terra-dialog-modal-header-background-color': '#c00',
         '--terra-dialog-modal-header-border': '1rem solid #c00',
-        '--terra-dialog-modal-body-color': '#000',
-        '--terra-dialog-modal-body-padding': '0rem',
-        '--terra-dialog-modal-footer-color': '#c00',
+        '--terra-dialog-modal-main-background-color': '#000',
+        '--terra-dialog-modal-main-padding': '0rem',
+        '--terra-dialog-modal-body-footer-background-color': '#c00',
         '--terra-dialog-modal-footer-border': '1rem solid #c00',
       },
     });
