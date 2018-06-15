faa0b83240489e98f7506743959fda236b9dbbc8
Tom Wu <tomleewu@gmail.com>
2018-06-15 12:18:26 -0500

    Added custom classNames to be appended to terra-theme-provider

--- packages/terra-theme-provider/tests/jest/ThemeProvider.test.jsx
+++ packages/terra-theme-provider/tests/jest/ThemeProvider.test.jsx
@@ -8,6 +8,12 @@ describe('ThemeProvider', () => {
     </ThemeProvider>
   );
 
+  const customClassRender = (
+    <ThemeProvider themeName={ThemeProvider.Opts.Themes.MOCK} className="customClass" >
+      <p> Child content </p>
+    </ThemeProvider>
+  );
+
   // Snapshot Tests
   it('should shallow render a theme provider component', () => {
     const wrapper = shallow(defaultRender);
@@ -16,6 +22,11 @@ describe('ThemeProvider', () => {
 
   it('should render a theme provider component', () => {
     const wrapper = render(defaultRender);
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('should render a theme with a custom class', () => {
+    const wrapper = shallow(customClassRender);
     expect(wrapper).toMatchSnapshot();
   });
 
