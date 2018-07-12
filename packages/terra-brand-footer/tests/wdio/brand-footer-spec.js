63c9d5be6680abf0aa2623ba4d3dfacc1c36a9c1
Nathan Faltermeier <nefalt2@gmail.com>
2018-07-12 17:26:49 -0500

    Absolutely everything is themeable

--- packages/terra-brand-footer/tests/wdio/brand-footer-spec.js
+++ packages/terra-brand-footer/tests/wdio/brand-footer-spec.js
@@ -5,41 +5,82 @@ const rules = { bypass: { enabled: false } };
 
 describe('BrandFooter', () => {
   describe('Default', () => {
-    beforeEach(() => browser.url('/#/raw/tests/terra-brand-footer/brand-footer/default-brand-footer'));
+    before(() => browser.url('/#/raw/tests/terra-brand-footer/brand-footer/default-brand-footer'));
 
     Terra.should.beAccessible({ viewports, rules });
     Terra.should.matchScreenshot({ viewports });
-    Terra.should.themeEachCustomProperty({
-      '--terra-brand-footer-nav-background-color': 'pink',
-      '--terra-brand-footer-nav-color': 'yellow',
-      '--terra-brand-footer-link-color': 'green',
-      '--terra-brand-footer-footer-content-background-color': 'red',
-      '--terra-brand-footer-footer-content-color': 'black',
+    Terra.should.themeCombinationOfCustomProperties({
+      testName: 'themed',
+      selector: '#wrapper',
+      properties: {
+        '--terra-brand-footer-margin': '4px',
+        '--terra-brand-footer-nav-background-color': 'pink',
+        '--terra-brand-footer-nav-color': 'yellow',
+        '--terra-brand-footer-nav-header-color': 'orange',
+        '--terra-brand-footer-link-color': 'green',
+        '--terra-brand-footer-footer-content-background-color': 'red',
+        '--terra-brand-footer-footer-content-color': 'black',
+        '--terra-brand-footer-nav-padding': '3px',
+        '--terra-brand-footer-horizontal-nav-first-link-padding-right-before': '5px',
+        '--terra-brand-footer-horizontal-nav-link-padding-before': '5px',
+        '--terra-brand-footer-link-group-margin': '20px',
+        '--terra-brand-footer-nav-link-font-size': '8px',
+        '--terra-brand-footer-nav-link-font-weight': '750',
+        '--terra-brand-footer-nav-link-line-height': '1',
+        '--terra-brand-footer-link-text-decoration': 'green wavy underline',
+        '--terra-brand-footer-footer-content-padding': '12px',
+        '--terra-brand-footer-floating-content-margin-bottom': '18px',
+        '--terra-brand-footer-content-bottom-margin-top': '12px',
+      },
     });
   });
 
   describe('Links Prop', () => {
-    beforeEach(() => browser.url('/#/raw/tests/terra-brand-footer/brand-footer/links-prop-brand-footer'));
+    before(() => browser.url('/#/raw/tests/terra-brand-footer/brand-footer/links-prop-brand-footer'));
 
     Terra.should.beAccessible({ viewports, rules });
     Terra.should.matchScreenshot({ viewports });
-    Terra.should.themeEachCustomProperty({
-      '--terra-brand-footer-nav-background-color': 'pink',
-      '--terra-brand-footer-nav-color': 'yellow',
-      '--terra-brand-footer-link-color': 'green',
-      '--terra-brand-footer-footer-content-background-color': 'red',
-      '--terra-brand-footer-footer-content-color': 'black',
+    Terra.should.themeCombinationOfCustomProperties({
+      testName: 'themed',
+      selector: '#wrapper',
+      properties: {
+        '--terra-brand-footer-margin': '4px',
+        '--terra-brand-footer-nav-background-color': 'pink',
+        '--terra-brand-footer-nav-color': 'yellow',
+        '--terra-brand-footer-nav-header-color': 'orange',
+        '--terra-brand-footer-link-color': 'green',
+        '--terra-brand-footer-footer-content-background-color': 'red',
+        '--terra-brand-footer-footer-content-color': 'black',
+        '--terra-brand-footer-nav-padding': '3px',
+        '--terra-brand-footer-horizontal-nav-first-link-padding-right-before': '5px',
+        '--terra-brand-footer-horizontal-nav-link-padding-before': '5px',
+        '--terra-brand-footer-link-group-margin': '20px',
+        '--terra-brand-footer-nav-link-font-size': '8px',
+        '--terra-brand-footer-nav-link-font-weight': '750',
+        '--terra-brand-footer-nav-link-line-height': '1',
+        '--terra-brand-footer-link-text-decoration': 'green wavy underline',
+        '--terra-brand-footer-footer-content-padding': '12px',
+        '--terra-brand-footer-floating-content-margin-bottom': '18px',
+        '--terra-brand-footer-content-bottom-margin-top': '12px',
+      },
     });
   });
 
   describe('No Nav', () => {
-    beforeEach(() => browser.url('/#/raw/tests/terra-brand-footer/brand-footer/no-nav-brand-footer'));
+    before(() => browser.url('/#/raw/tests/terra-brand-footer/brand-footer/no-nav-brand-footer'));
 
     Terra.should.beAccessible({ viewports, rules });
     Terra.should.matchScreenshot({ viewports });
-    Terra.should.themeEachCustomProperty({
-      '--terra-brand-footer-footer-content-background-color': 'red',
-      '--terra-brand-footer-footer-content-color': 'black',
+    Terra.should.themeCombinationOfCustomProperties({
+      testName: 'themed',
+      properties: {
+        '--terra-brand-footer-margin': '4px',
+        '--terra-brand-footer-footer-content-background-color': 'red',
+        '--terra-brand-footer-footer-content-color': 'black',
+        '--terra-brand-footer-footer-content-padding': '12px',
+        '--terra-brand-footer-floating-content-margin-bottom': '18px',
+        '--terra-brand-footer-content-bottom-margin-top': '12px',
+      },
     });
   });
 
@@ -48,23 +89,55 @@ describe('BrandFooter', () => {
 
     Terra.should.beAccessible({ viewports, rules });
     Terra.should.matchScreenshot({ viewports });
-    Terra.should.themeEachCustomProperty({
-      '--terra-brand-footer-nav-header-color': 'orange',
+    Terra.should.themeCombinationOfCustomProperties({
+      testName: 'themed',
+      properties: {
+        '--terra-brand-footer-nav-header-font-size': '10px',
+        '--terra-brand-footer-nav-header-font-weight': '150',
+        '--terra-brand-footer-nav-header-line-height': '1',
+        '--terra-brand-footer-nav-header-text-transform': 'none',
+        '--terra-brand-footer-nav-link-font-size': '8px',
+        '--terra-brand-footer-nav-link-font-weight': '750',
+        '--terra-brand-footer-nav-link-line-height': '1',
+      },
     });
   });
 
   describe('Vertical Nav', () => {
-    beforeEach(() => browser.url('/#/raw/tests/terra-brand-footer/brand-footer/vertical-nav-brand-footer'));
+    before(() => browser.url('/#/raw/tests/terra-brand-footer/brand-footer/vertical-nav-brand-footer'));
 
     Terra.should.beAccessible({ viewports, rules });
     Terra.should.matchScreenshot({ viewports });
-    Terra.should.themeEachCustomProperty({
-      '--terra-brand-footer-nav-background-color': 'pink',
-      '--terra-brand-footer-nav-color': 'yellow',
-      '--terra-brand-footer-nav-header-color': 'orange',
-      '--terra-brand-footer-link-color': 'green',
-      '--terra-brand-footer-footer-content-background-color': 'red',
-      '--terra-brand-footer-footer-content-color': 'black',
+    Terra.should.themeCombinationOfCustomProperties({
+      testName: 'themed',
+      properties: {
+        '--terra-brand-footer-margin': '4px',
+        '--terra-brand-footer-nav-background-color': 'pink',
+        '--terra-brand-footer-nav-color': 'yellow',
+        '--terra-brand-footer-nav-header-color': 'orange',
+        '--terra-brand-footer-link-color': 'green',
+        '--terra-brand-footer-footer-content-background-color': 'red',
+        '--terra-brand-footer-footer-content-color': 'black',
+        '--terra-brand-footer-nav-padding': '3px',
+        '--terra-brand-footer-vertical-nav-link-group-min-width': '120px',
+        '--terra-brand-footer-vertical-nav-link-group-padding-right': '2px',
+        '--terra-brand-footer-vertical-nav-link-padding-bottom': '1px',
+        '--terra-brand-footer-vertical-nav-link-padding-top': '1px',
+        '--terra-brand-footer-vertical-nav-header-padding-bottom': '1px',
+        '--terra-brand-footer-vertical-nav-header-padding-top': '1px',
+        '--terra-brand-footer-link-group-margin': '20px',
+        '--terra-brand-footer-nav-header-font-size': '10px',
+        '--terra-brand-footer-nav-header-font-weight': '150',
+        '--terra-brand-footer-nav-header-line-height': '1',
+        '--terra-brand-footer-nav-header-text-transform': 'none',
+        '--terra-brand-footer-nav-link-font-size': '8px',
+        '--terra-brand-footer-nav-link-font-weight': '750',
+        '--terra-brand-footer-nav-link-line-height': '1',
+        '--terra-brand-footer-link-text-decoration': 'green wavy underline',
+        '--terra-brand-footer-footer-content-padding': '12px',
+        '--terra-brand-footer-floating-content-margin-bottom': '18px',
+        '--terra-brand-footer-content-bottom-margin-top': '12px',
+      },
     });
   });
 
@@ -82,8 +155,8 @@ describe('BrandFooter', () => {
     Terra.should.matchScreenshot({ viewports });
   });
 
-  describe('Many Sections', () => {
-    before(() => browser.url('/#/raw/tests/terra-brand-footer/brand-footer/many-sections-brand-footer'));
+  describe('Many Link Groups', () => {
+    before(() => browser.url('/#/raw/tests/terra-brand-footer/brand-footer/many-link-groups-brand-footer'));
 
     // Medium exemplifies 3x2 layout and Large 4 in a row with 2 on the botton row
     Terra.should.beAccessible({ viewports: Terra.viewports('medium', 'large'), rules });
