f8f7500f53fd3ccd2c60d0ae99d49e80a2b4d35f
Derek Yu <yuderekyu@gmail.com>
2018-10-03 10:10:49 -0500

    [terra-application-name] remove background color from menu test component.

--- packages/terra-application-name/src/terra-dev-site/test/application-name/ApplicationMenuNameDefault.test.jsx
+++ packages/terra-application-name/src/terra-dev-site/test/application-name/ApplicationMenuNameDefault.test.jsx
@@ -3,7 +3,7 @@ import Image from 'terra-image';
 import ApplicationMenuName from '../../../ApplicationMenuName';
 
 export default () => (
-  <div style={{ backgroundColor: '0079be', width: '100%' }}>
+  <div style={{ width: '100%' }}>
     <ApplicationMenuName
       id="default"
       title="Title"
