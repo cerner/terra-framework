4cc085c9dd34f81e5a400067f96c683e96c6b183
Derek Yu <yuderekyu@gmail.com>
2018-10-05 13:26:30 -0500

    [terra-application-utility] update theme tests to test combinations of theme values. speeds tests up :)

--- packages/terra-application-utility/tests/wdio/application-header-utility-spec.js
+++ packages/terra-application-utility/tests/wdio/application-header-utility-spec.js
@@ -9,20 +9,24 @@ describe('ApplicationHeaderUtility', () => {
   describe('Displays a default application header utility', () => {
     Terra.should.matchScreenshot({ viewports, selector: '#default' });
     Terra.should.beAccessible({ viewports, context: '#default' });
-    Terra.should.themeEachCustomProperty('#default', {
-      '--terra-application-header-utility-background-color': 'blue',
-      '--terra-application-header-utility-border-radius': '6px',
-      '--terra-application-header-utility-border-width': '10px',
-      '--terra-application-header-utility-color': 'purple',
-      '--terra-application-header-utility-padding-left': '20px',
-      '--terra-application-header-utility-padding-right': '20px',
-      '--terra-application-header-utility-icon-min-width': '15px',
-      '--terra-application-header-utility-accessory-height': '2rem',
-      '--terra-application-header-utility-accessory-margin-right': '20px',
-      '--terra-application-header-utility-accessory-width': '2rem',
-      '--terra-application-header-utility-title-font-size': '2rem',
-      '--terra-application-header-utility-title-font-weight': 'bold',
-      '--terra-application-header-utility-title-margin-right': '20px',
+    Terra.should.themeCombinationOfCustomProperties({
+      testName: 'themed',
+      selector: '#default',
+      properties: {
+        '--terra-application-header-utility-background-color': 'blue',
+        '--terra-application-header-utility-border-radius': '6px',
+        '--terra-application-header-utility-border-width': '10px',
+        '--terra-application-header-utility-color': 'purple',
+        '--terra-application-header-utility-padding-left': '20px',
+        '--terra-application-header-utility-padding-right': '20px',
+        '--terra-application-header-utility-icon-min-width': '15px',
+        '--terra-application-header-utility-accessory-height': '2rem',
+        '--terra-application-header-utility-accessory-margin-right': '20px',
+        '--terra-application-header-utility-accessory-width': '2rem',
+        '--terra-application-header-utility-title-font-size': '2rem',
+        '--terra-application-header-utility-title-font-weight': 'bold',
+        '--terra-application-header-utility-title-margin-right': '20px',
+      },
     });
   });
 
