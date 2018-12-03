e5f1ce95f7d5ef604f19cee531f2f6adf23797a9
Rohrbough,Emily <Emily.Rohrbough@cerner.com>
2018-12-03 13:31:46 -0600

    Update repo references in docs

--- packages/terra-application-name/src/terra-dev-site/test/application-name/ApplicationHeaderNameDefault.test.jsx
+++ packages/terra-application-name/src/terra-dev-site/test/application-name/ApplicationHeaderNameDefault.test.jsx
@@ -7,7 +7,7 @@ export default () => (
     <ApplicationHeaderName
       id="default"
       title="Title"
-      accessory={<Image alt="terra accessory" variant="rounded" src="https://github.com/cerner/terra-core/raw/master/terra.png" height="26px" width="26px" />}
+      accessory={<Image alt="terra accessory" variant="rounded" src="https://github.com/cerner/terra-framework/raw/master/terra.png" height="26px" width="26px" />}
     />
   </div>
 );
