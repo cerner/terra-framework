bdd7c22bd35e6fbcc4be4f43db8d1509ef8ad2f0
neilpfeiffer <neil.pfeiffer@cerner.com>
2018-12-13 06:51:02 -0600

    [terra-application-name] update layout for theming

--- packages/terra-application-name/src/terra-dev-site/test/application-name/ApplicationMenuNameDefault.test.jsx
+++ packages/terra-application-name/src/terra-dev-site/test/application-name/ApplicationMenuNameDefault.test.jsx
@@ -3,7 +3,7 @@ import Image from 'terra-image';
 import ApplicationMenuName from '../../../ApplicationMenuName';
 
 export default () => (
-  <div style={{ width: '100%' }}>
+  <div style={{ width: '100%', backgroundColor: 'green' }}>
     <ApplicationMenuName
       id="default"
       title="Title"
