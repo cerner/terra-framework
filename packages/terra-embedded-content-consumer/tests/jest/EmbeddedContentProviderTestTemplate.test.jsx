dc544d4bcf876d7540acb5f4ecc664be0c615bb8
Rohrbough,Emily <Emily.Rohrbough@cerner.com>
2019-03-12 11:53:11 -0500

    whoops jest test for props that were removed

--- packages/terra-embedded-content-consumer/tests/jest/EmbeddedContentProviderTestTemplate.test.jsx
+++ packages/terra-embedded-content-consumer/tests/jest/EmbeddedContentProviderTestTemplate.test.jsx
@@ -23,20 +23,3 @@ it('should render the provider test template with a custom class name', () => {
   const wrapper = render(providerExample);
   expect(wrapper).toMatchSnapshot();
 });
-
-it('should accept onMount and onUnmount props', () => {
-  const onMount = () => 'example mounted';
-  const onUnmount = () => 'example will unmount';
-
-  const providerExample = (
-    <ProviderTestTemplate onMount={onMount} onUnmount={onUnmount}>
-      <div>Content to display</div>
-    </ProviderTestTemplate>
-  );
-
-  const wrapper = mount(providerExample);
-
-  expect(wrapper.instance().props.onMount).toBe(onMount);
-  expect(wrapper.instance().props.onUnmount).toBe(onUnmount);
-  expect(wrapper).toMatchSnapshot();
-});
