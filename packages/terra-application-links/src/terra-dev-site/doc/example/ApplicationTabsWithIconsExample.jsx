65bbbef3439589d45edc9cef0cf0c7949fb5be65
Cody Price <Cody.Price@cerner.com>
2019-03-19 13:16:59 -0500

    add tabs with icons example

---
+++ packages/terra-application-links/src/terra-dev-site/doc/example/ApplicationTabsWithIconsExample.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+/* eslint-disable import/no-extraneous-dependencies, import/no-unresolved, import/extensions */
+import { ApplicationTabs } from 'terra-application-links/lib/ApplicationLinks';
+import ApplicationLinkConfig from 'terra-application-links/lib/terra-dev-site/doc/common/ApplicationLinksWithIconsConfig';
+/* eslint-enable import/no-extraneous-dependencies, import/no-unresolved, import/extensions */
+
+const demoheaderstyles = {
+  width: '100%',
+  backgroundColor: '#8ca1b1',
+  height: '72px',
+  position: 'relative',
+};
+
+const ApplicationTabsExample = () => (
+  <div style={demoheaderstyles}>
+    <ApplicationTabs alignment="center" links={ApplicationLinkConfig} />
+  </div>
+);
+
+export default ApplicationTabsExample;
