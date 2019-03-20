c7d0290ec0191ceccd6fa6e4216379d51c965f78
Cody Price <Cody.Price@cerner.com>
2019-03-20 10:24:17 -0500

    address pr comments: add demoStyles to examples and tests for links

--- packages/terra-application-links/src/terra-dev-site/test/application-links/ApplicationTabsWithIcons.test.jsx
+++ packages/terra-application-links/src/terra-dev-site/test/application-links/ApplicationTabsWithIcons.test.jsx
@@ -2,20 +2,14 @@ import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
 import ApplicationTabs from '../../../tabs/ApplicationTabs';
 import testLinksWithIconsConfig from '../common/testLinksWithIconsConfig';
-
-const demoheaderstyles = {
-  width: '100%',
-  backgroundColor: '#1c5f92',
-  height: '72px',
-  position: 'relative',
-};
+import demoStyles from './demoStyles.scss';
 
 export default () => (
   <MemoryRouter
     initialEntries={testLinksWithIconsConfig.map(link => link.path)}
     initialIndex={0}
   >
-    <div style={demoheaderstyles}>
+    <div style={demoStyles['demo-header'] + demoStyles['demo-header-with-icons']}>
       <ApplicationTabs id="test-tabs" links={testLinksWithIconsConfig} />
     </div>
   </MemoryRouter>
