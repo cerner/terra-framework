ad3ae5750dcc76cd042d08212a2e95abe52e2671
Brett Jankord <brett.jankord@cerner.com>
2019-03-21 22:08:47 -0500

    Fix lint issue

--- packages/terra-menu/src/_MenuContent.jsx
+++ packages/terra-menu/src/_MenuContent.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+/* eslint-disable-next-line import/no-extraneous-dependencies */
 import { injectIntl, intlShape } from 'react-intl';
 import PropTypes from 'prop-types';
 import List from 'terra-list';
