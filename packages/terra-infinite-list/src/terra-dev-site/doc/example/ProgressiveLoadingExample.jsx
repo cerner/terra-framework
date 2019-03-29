9f27f86e946dba5f964019f7fef9b617e687482d
Dave Kasper <dave.m.kasper@gmail.com>
2019-03-29 07:56:26 -0500

    [terra-infinite-list] Update documentation for headers and subheaders (#546)

---
+++ packages/terra-infinite-list/src/terra-dev-site/doc/example/ProgressiveLoadingExample.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+/* eslint-disable import/no-extraneous-dependencies, import/no-unresolved, import/extensions */
+import InfiniteList, { Item } from 'terra-infinite-list/lib/index';
+import Placeholder from 'terra-doc-template/lib/Placeholder';
+/* eslint-enable import/no-extraneous-dependencies, import/no-unresolved, import/extensions */
+
+import MyProgressiveLoadingIndicator from '../common/MyProgressiveLoadingIndicator';
+import MyExampleContainer from '../common/MyExampleContainer';
+
+const ProgressiveLoadingExample = () => (
+  <MyExampleContainer>
+    <InfiniteList
+      dividerStyle="standard"
+      progressiveLoadingIndicator={<MyProgressiveLoadingIndicator />}
+    >
+      <Item key="item-1">
+        <Placeholder title="Item 1" style={{ height: '50px' }} />
+      </Item>
+    </InfiniteList>
+  </MyExampleContainer>
+);
+
+export default ProgressiveLoadingExample;
--- packages/terra-infinite-list/src/terra-dev-site/doc/infinite-list/InfiniteList.1.doc.jsx
+++ packages/terra-infinite-list/src/terra-dev-site/doc/infinite-list/InfiniteList.1.doc.jsx
@@ -6,10 +6,14 @@ import ReadMe from '../../../../docs/README.md';
 /* eslint-disable import/no-webpack-loader-syntax, import/first, import/extensions, import/no-unresolved, import/no-duplicates */
 // Component Source
 import InfiniteListSrc from '!raw-loader!../../../../src/InfiniteList.jsx';
-import InfiniteListExampleSrc from '!raw-loader!../../../../src/terra-dev-site/doc/example/InfiniteListExample.jsx';
+import InitialLoadingExampleSrc from '!raw-loader!../../../../src/terra-dev-site/doc/example/InitialLoadingExample.jsx';
+import ProgressiveLoadingExampleSrc from '!raw-loader!../../../../src/terra-dev-site/doc/example/ProgressiveLoadingExample.jsx';
+import VirtualizedExampleSrc from '!raw-loader!../../../../src/terra-dev-site/doc/example/VirtualizedExample.jsx';
 
 // Example Files
-import InfiniteListExample from '../example/InfiniteListExample';
+import InitialLoadingExample from '../example/InitialLoadingExample';
+import ProgressiveLoadingExample from '../example/ProgressiveLoadingExample';
+import VirtualizedExample from '../example/VirtualizedExample';
 /* eslint-enabled import/no-webpack-loader-syntax, import/first, import/extensions, import/no-unresolved, import/no-duplicates */
 
 const DocPage = () => (
@@ -19,9 +23,19 @@ const DocPage = () => (
     srcPath={`https://github.com/cerner/terra-framework/tree/master/packages/${name}`}
     examples={[
       {
-        title: 'Example Infinite List',
-        example: <InfiniteListExample />,
-        source: InfiniteListExampleSrc,
+        title: 'Example Initial Loading Indicator',
+        example: <InitialLoadingExample />,
+        source: InitialLoadingExampleSrc,
+      },
+      {
+        title: 'Example Progressive Loading Indicator',
+        example: <ProgressiveLoadingExample />,
+        source: ProgressiveLoadingExampleSrc,
+      },
+      {
+        title: 'Example Virtualized List',
+        example: <VirtualizedExample />,
+        source: VirtualizedExampleSrc,
       },
     ]}
     propsTables={[
