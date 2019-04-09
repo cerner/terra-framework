c5e38b6ab26fb60a6a51deb582a575080ea78de0
Brett Jankord <brett.jankord@cerner.com>
2019-04-09 12:44:02 -0500

    Clean up JSDoc comments

--- packages/terra-date-picker/src/DatePositionUtil.js
+++ packages/terra-date-picker/src/DatePositionUtil.js
@@ -1,24 +1,24 @@
 /**
  * Calculates the bottom position of the element.
  *
- * @ param {Object} rect - The element's size and position relative to the viewport.
+ * @param {Object} rect - The element's size and position relative to the viewport.
  */
 const calculateBottomPosition = rect => (rect.top + rect.height);
 
 /**
  * Calculates the right position of the element.
  *
- * @ param {Object} rect - The element's size and position relative to the viewport.
+ * @param {Object} rect - The element's size and position relative to the viewport.
  */
 const calculateRightPosition = rect => (rect.left + rect.width);
 
 /**
  * Checks if the arrow offset fits within both the left and right bounds of the target.
  *
- * @ param {Object} contentRect - The content element's size and position relative to the viewport.
- * @ param {Object} targetRect -  The target element's size and position relative to the viewport.
- * @ param {number} arrowOffset - Half the base of the arrow.
- * @ param {number} cornerOffset - The rounded corner size of the content.
+ * @param {Object} contentRect - The content element's size and position relative to the viewport.
+ * @param {Object} targetRect -  The target element's size and position relative to the viewport.
+ * @param {number} arrowOffset - Half the base of the arrow.
+ * @param {number} cornerOffset - The rounded corner size of the content.
  */
 const doesArrowFitHorizontal = (contentRect, targetRect, arrowOffset, cornerOffset) => {
   const fitsLeftBound = calculateRightPosition(contentRect) - arrowOffset - cornerOffset >= targetRect.left;
@@ -30,10 +30,10 @@ const doesArrowFitHorizontal = (contentRect, targetRect, arrowOffset, cornerOffs
 /**
  * This method calculates the arrow position based on the content and target's relative positions.
  *
- * @ param {Object} contentPosition - The content x/y coords, rectangle, offset, and attachment.
- * @ param {Object} targetPosition - The target x/y coords, rectangle, offset, and attachment.
- * @ param {number} arrowOffset - Half the base of the arrow.
- * @ param {number} cornerOffset - The rounded corner size of the content.
+ * @param {Object} contentPosition - The content x/y coords, rectangle, offset, and attachment.
+ * @param {Object} targetPosition - The target x/y coords, rectangle, offset, and attachment.
+ * @param {number} arrowOffset - Half the base of the arrow.
+ * @param {number} cornerOffset - The rounded corner size of the content.
  */
 const getArrowPosition = (contentPosition, targetPosition, arrowOffset, cornerOffset) => {
   const contentRect = contentPosition.rect;
