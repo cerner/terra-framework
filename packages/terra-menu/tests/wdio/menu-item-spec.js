5ae5775b963d09afdee418e20119a328f72fa49b
Alex Brisimitzakis <alex.brisimitzakis@cerner.com>
2019-04-25 17:14:08 -0500

    Fixed hyphen issue causing failing wdio test

--- packages/terra-menu/tests/wdio/menu-item-spec.js
+++ packages/terra-menu/tests/wdio/menu-item-spec.js
@@ -15,7 +15,7 @@ describe('Menu Item', () => {
       properties: {
         '--terra-menu-item-text-color': 'red',
         '--terra-menu-item-text-font-size': '20px',
-        '—-terra-menu-item-text-font-weight': 'lighter',
+        '--terra-menu-item-text-font-weight': 'bold',
       },
     });
   });
