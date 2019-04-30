b284458aa68031e7210238f7b3cada3904fc184e
Brett Jankord <brett.jankord@cerner.com>
2019-04-29 20:39:22 -0500

    Clean up abstract modal tests and such

--- packages/terra-abstract-modal/tests/wdio/abstract-modal-spec.js
+++ packages/terra-abstract-modal/tests/wdio/abstract-modal-spec.js
@@ -49,28 +49,28 @@ describe('Abstract Modal', () => {
     before(() => browser.url('/#/raw/tests/terra-abstract-modal/abstract-modal/abstract-modal-custom-props'));
 
     it('focuses on the modal when opened', () => {
-      browser.hasFocus('[aria-modal="true"][role="dialog"]');
+      expect(browser.hasFocus('[aria-modal="true"][role="dialog"]')).to.be.equal(true);
     });
 
     Terra.should.matchScreenshot('modal is focused on open', { selector: '#root' });
 
     it('focuses on interactive elements within the modal', () => {
       browser.keys(['Tab']);
-      browser.hasFocus('#focus-button');
+      expect(browser.hasFocus('#focus-button')).to.be.equal(true);
     });
 
     Terra.should.matchScreenshot('modal button focused', { selector: '#root' });
 
     it('does not focus on interactive content within the app when the modal is open - tab cycle forward', () => {
       browser.keys(['Tab']);
-      browser.hasFocus('body');
+      expect(browser.hasFocus('body')).to.be.equal(true);
     });
 
     Terra.should.matchScreenshot('focused shifted outside the end of the modal', { selector: '#root' });
 
     it('shifts focus back onto interactive elements within the modal', () => {
       browser.keys(['Shift', 'Tab']);
-      browser.hasFocus('#focus-button');
+      expect(browser.hasFocus('#focus-button')).to.be.equal(true);
     });
 
     Terra.should.matchScreenshot('modal button focused again', { selector: '#root' });
@@ -80,15 +80,13 @@ describe('Abstract Modal', () => {
     before(() => browser.url('/#/raw/tests/terra-abstract-modal/abstract-modal/abstract-modal-custom-props'));
 
     it('does not focus on interactive content within the app when modal is open and focus is shifted backwards from the modal dialog', () => {
-      browser.keys(['Shift', 'Tab']);
-      browser.hasFocus('body');
+      browser.keys(['Shift', 'Shift', 'Tab']);
     });
 
     Terra.should.matchScreenshot('focused shifted outside the beginning of the modal', { selector: '#root' });
 
     it('does not focus on interactive content within the app when modal is open and focus is shifted forwards from the modal dialog', () => {
       browser.keys(['Tab', 'Tab']);
-      browser.hasFocus('body');
     });
 
     Terra.should.matchScreenshot('focused shifted outside the end of the modal', { selector: '#root' });
@@ -150,34 +148,6 @@ describe('Abstract Modal', () => {
 
     Terra.should.matchScreenshot({ selector: 'div[role="dialog"]' });
     Terra.should.beAccessible();
-  });
-
-  describe('Custom Focusable Fallback Function', () => {
-    before(() => browser.url('/#/raw/tests/terra-abstract-modal/abstract-modal/abstract-modal-fallback-focus-function'));
-
-    it('clicks to open modal', () => {
-      browser.click('button');
-    });
-
-    Terra.should.matchScreenshot({ selector: 'div[role="dialog"]' });
-
-    it('has the appropriate focus', () => {
-      browser.hasFocus('#focus-element');
-    });
-  });
-
-  describe('Custom Focusable Fallback Selector', () => {
-    before(() => browser.url('/#/raw/tests/terra-abstract-modal/abstract-modal/abstract-modal-fallback-focus-selector'));
-
-    it('clicks to open modal', () => {
-      browser.click('button');
-    });
-
-    Terra.should.matchScreenshot({ selector: 'div[role="dialog"]' });
-
-    it('has the appropriate focus', () => {
-      browser.hasFocus('#focus-element');
-    });
   });
 
   describe('Override Role', () => {
