df0a753008b1f424615a0e373aae9f6d3092b76e
Kasper,Dave <dave.m.kasper@gmail.com>
2019-05-09 15:18:37 -0500

    Remove commented code.

--- packages/terra-application-navigation/tests/jest/common/PopupMenu.test.jsx
+++ packages/terra-application-navigation/tests/jest/common/PopupMenu.test.jsx
@@ -1,52 +1,6 @@
 import React from 'react';
 import PopupMenu from '../../../src/common/_PopupMenu';
 
-//   /**
-//    * A configuration object with information pertaining to the application's user.
-//    */
-//   userConfig: userConfigPropType,
-//   /**
-//    * An array of selectable items to be displayed within the popup.
-//    */
-//   menuItems: PropTypes.arrayOf(PropTypes.shape({
-//     /**
-//      * Icon to be rendered
-//      */
-//     icon: PropTypes.element,
-//     /**
-//      * Key used as react key, and returned in the onSelect
-//      */
-//     key: PropTypes.string.isRequired,
-//     /**
-//      * Object to be returned in the onSelect
-//      */
-//     metaData: PropTypes.object,
-//     /**
-//      * Text display and/or aria-label
-//      */
-//     text: PropTypes.string.isRequired,
-//     /**
-//      * Whether or not this item is the active item.
-//      */
-//     isActive: PropTypes.bool,
-//     /**
-//      * The number value representing the notification count.
-//      */
-//     notificationCount: PropTypes.number,
-//   })),
-//   /**
-//    * Function callback for selection of the menu item. Returns (key, metaData).
-//    */
-//   onSelectMenuItem: PropTypes.func,
-//   /**
-//    * Whehther or not content is be constrained vertically.
-//    */
-//   isHeightBounded: PropTypes.bool,
-//   /**
-//    * Whehther or not selected states should display on the menu item.
-//    */
-//   showSelections: PropTypes.bool,
-
 describe('PopupMenu', () => {
   it('should render default element', () => {
     const shallowComponent = shallow(
