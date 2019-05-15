7a262d2bd5402c81396ab1fc352fe30c90088508
Rohrbough,Emily <Emily.Rohrbough@cerner.com>
2019-05-15 16:50:56 -0500

    use validateElement

--- packages/terra-application-utility/tests/wdio/application-header-utility-spec.js
+++ packages/terra-application-utility/tests/wdio/application-header-utility-spec.js
@@ -1,50 +1,58 @@
 const viewports = Terra.viewports('tiny', 'medium');
 
-describe('ApplicationHeaderUtility', () => {
-  beforeEach(() => {
-    browser.url('/#/raw/tests/terra-application-utility/application-utility/default-application-header-utility');
-    browser.waitForVisible('#default');
-  });
+viewports.forEach((viewport) => {
+  describe(`ApplicationHeaderUtility - ${viewport}`, () => {
+    before(() => browser.setViewportSize(viewport));
 
-  describe('Displays a default application header utility', () => {
-    Terra.should.matchScreenshot({ viewports, selector: '#default' });
-    Terra.should.beAccessible({ viewports, context: '#default' });
-    Terra.should.themeCombinationOfCustomProperties({
-      testName: 'themed',
-      selector: '#default',
-      properties: {
-        '--terra-application-utility-header-background-color': 'blue',
-        '--terra-application-utility-header-border-radius': '6px',
-        '--terra-application-utility-header-border-width': '10px',
-        '--terra-application-utility-header-color': 'purple',
-        '--terra-application-utility-header-padding-left': '20px',
-        '--terra-application-utility-header-padding-right': '20px',
-        '--terra-application-utility-header-icon-width': '15px',
-        '--terra-application-utility-header-accessory-height': '2rem',
-        '--terra-application-utility-header-accessory-margin-right': '20px',
-        '--terra-application-utility-header-accessory-width': '2rem',
-        '--terra-application-utility-header-title-font-size': '2rem',
-        '--terra-application-utility-header-title-font-weight': 'bold',
-        '--terra-application-utility-header-title-margin-right': '20px',
-      },
+    beforeEach(() => {
+      browser.url('/#/raw/tests/terra-application-utility/application-utility/default-application-header-utility');
+      browser.waitForVisible('#default');
     });
-  });
 
-  describe('Hover-application header utility', () => {
-    beforeEach(() => { browser.moveToObject('#default'); });
-    Terra.should.matchScreenshot({ viewports, selector: '#default' });
-    Terra.should.beAccessible({ viewports, context: '#default' });
-    Terra.should.themeEachCustomProperty('#default', {
-      '--terra-application-utility-header-hover-background-color': 'blue',
+    describe('Displays a default application header utility', () => {
+      Terra.should.validateElement({ selector: '#default' });
+      if (browser.options.formFactor === 'medium') {
+
+        Terra.should.themeCombinationOfCustomProperties({
+          testName: 'themed',
+          selector: '#default',
+          properties: {
+            '--terra-application-utility-header-background-color': 'blue',
+            '--terra-application-utility-header-border-radius': '6px',
+            '--terra-application-utility-header-border-width': '10px',
+            '--terra-application-utility-header-color': 'purple',
+            '--terra-application-utility-header-padding-left': '20px',
+            '--terra-application-utility-header-padding-right': '20px',
+            '--terra-application-utility-header-icon-width': '15px',
+            '--terra-application-utility-header-accessory-height': '2rem',
+            '--terra-application-utility-header-accessory-margin-right': '20px',
+            '--terra-application-utility-header-accessory-width': '2rem',
+            '--terra-application-utility-header-title-font-size': '2rem',
+            '--terra-application-utility-header-title-font-weight': 'bold',
+            '--terra-application-utility-header-title-margin-right': '20px',
+          },
+        });
+      }
     });
-  });
 
-  describe('Focus-application header utility', () => {
-    beforeEach(() => { browser.keys('Tab'); });
-    Terra.should.matchScreenshot({ viewports, selector: '#default' });
-    Terra.should.beAccessible({ viewports, context: '#default' });
-    Terra.should.themeEachCustomProperty('#default', {
-      '--terra-application-utility-header-focus-box-shadow': '0 0 4px 4px rgba(80, 80, 233, 0.5), 0 0 6px 7px rgba(80, 80, 233, 0.35)',
+    describe('Hover-application header utility', () => {
+      beforeEach(() => { browser.moveToObject('#default'); });
+      Terra.should.validateElement({ selector: '#default' });
+      if (browser.options.formFactor === 'medium') {
+        Terra.should.themeEachCustomProperty('#default', {
+          '--terra-application-utility-header-hover-background-color': 'blue',
+        });
+      }
+    });
+
+    describe('Focus-application header utility', () => {
+      beforeEach(() => { browser.keys('Tab'); });
+      Terra.should.validateElement({ selector: '#default' });
+      if (browser.options.formFactor === 'medium') {
+        Terra.should.themeEachCustomProperty('#default', {
+          '--terra-application-utility-header-focus-box-shadow': '0 0 4px 4px rgba(80, 80, 233, 0.5), 0 0 6px 7px rgba(80, 80, 233, 0.35)',
+        });
+      }
     });
   });
 });
