1b11083809ede5a2d8ac5705715d62aa75c577bb
Emily Rohrbough <emilyrohrbough@users.noreply.github.com>
2019-05-16 10:06:58 -0500

    use validate element (#672)

--- packages/terra-application-name/tests/wdio/application-header-name-spec.js
+++ packages/terra-application-name/tests/wdio/application-header-name-spec.js
@@ -1,27 +1,32 @@
 const viewports = Terra.viewports('tiny', 'medium');
 
-describe('ApplicationHeaderName', () => {
-  describe('Displays a default application header name', () => {
-    beforeEach(() => browser.url('/#/raw/tests/terra-application-name/application-name/application-header-name-default'));
+viewports.forEach((viewport) => {
+  describe(`ApplicationHeaderName - ${viewport}`, () => {
+    before(() => browser.setViewportSize(viewport));
 
-    Terra.should.matchScreenshot({ viewports, selector: '#default' });
-    Terra.should.beAccessible({ viewports, context: '#default' });
-    Terra.should.themeCombinationOfCustomProperties({
-      testName: 'themed',
-      properties: {
-        '--terra-application-name-header-align-items': 'flex-start',
-        '--terra-application-name-header-color': '#800080',
-        '--terra-application-name-header-accessory-padding-right': '1rem',
-        '--terra-application-name-header-title-font-weight': 'bold',
-        '--terra-application-name-header-padding-left': '1rem',
-        '--terra-application-name-header-padding-right': '3rem',
-      },
+    describe('Displays a default application header name', () => {
+      before(() => browser.url('/#/raw/tests/terra-application-name/application-name/application-header-name-default'));
+
+      Terra.should.validateElement({ selector: '#default' });
+      if (browser.options.formFactor === 'medium') {
+        Terra.should.themeCombinationOfCustomProperties({
+          testName: 'themed',
+          properties: {
+            '--terra-application-name-header-align-items': 'flex-start',
+            '--terra-application-name-header-color': '#800080',
+            '--terra-application-name-header-accessory-padding-right': '1rem',
+            '--terra-application-name-header-title-font-weight': 'bold',
+            '--terra-application-name-header-padding-left': '1rem',
+            '--terra-application-name-header-padding-right': '3rem',
+          },
+        });
+      }
     });
-  });
 
-  describe('Displays a truncated application header name', () => {
-    beforeEach(() => browser.url('/#/raw/tests/terra-application-name/application-name/application-header-name-truncated'));
+    describe('Displays a truncated application header name', () => {
+      before(() => browser.url('/#/raw/tests/terra-application-name/application-name/application-header-name-truncated'));
 
-    Terra.should.matchScreenshot({ viewports, selector: '#truncated' });
+      Terra.should.validateElement({ selector: '#truncated' });
+    });
   });
 });
