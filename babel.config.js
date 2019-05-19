5ac5ba0be4e686e396718a1840b2643fddbdb949
Matt Henkes <mjhenkes@gmail.com>
2019-05-19 09:50:15 -0500

    babel update

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,20 @@
-module.exports = () => ({
-  babelrcRoots: ['.', 'packages/*'],
-});
+module.exports = () => {
+  api.cache(false);
+  api.assertVersion('^7.4.4');
+
+  const presets = [
+    '@babel/preset-env',
+    '@babel/preset-react',
+  ];
+
+  const plugins = [
+    '@babel/plugin-transform-object-assign',
+    '@babel/plugin-proposal-object-rest-spread',
+  ];
+
+  return {
+    presets,
+    plugins,
+    babelrcRoots: ['.', 'packages/*'],
+  };
+};
