124ff7cc81ac3fe151bdfb8be7a8b051e0d6aa8f
Matt Henkes <mjhenkes@gmail.com>
2019-05-28 23:15:03 -0500

    fix terra-layout

--- packages/terra-layout/tests/wdio/layout-slide-panel-spec.js
+++ packages/terra-layout/tests/wdio/layout-slide-panel-spec.js
@@ -23,10 +23,7 @@ describe('Toggle the slide panel click', () => {
 describe('Toggle the slide panel double click', () => {
   before(() => browser.setViewportSize(Terra.viewports('medium')[0]));
   beforeEach(() => {
-    browser.url('/#/raw/tests/terra-layout/layout/layout-slide-panel-example');
     const toggleBtn1 = $('#test-toggle-1');
-    browser.click('#test-toggle-1');
-    browser.waitForExist('[class*=_panel_][aria-hidden="false"]');
     browser.pause(150);
     const toggleBtn2 = $('#test-toggle-2');
     expect(toggleBtn2.hasFocus()).to.be.equal(true);
