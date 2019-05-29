5645ae5324899241cd5a822c0ab13d73bd5274e3
Matt Henkes <mjhenkes@gmail.com>
2019-05-29 14:17:07 -0500

    fix tabs

--- packages/terra-tabs/tests/wdio/tabs-spec.js
+++ packages/terra-tabs/tests/wdio/tabs-spec.js
@@ -1,101 +1,82 @@
-const viewports = Terra.viewports('tiny', 'small', 'medium', 'large', 'huge', 'enormous');
-
 const ignoredA11y = {
   // https://github.com/cerner/terra-core/issues/1061
   'aria-allowed-attr': { enabled: false },
 };
 
 // Verify tabs collapse appropriately
-describe('Tabs - Responsive', () => {
-  viewports.forEach((viewport) => {
-    describe('Default', () => {
-      before(() => {
-        browser.url('/#/raw/tests/terra-tabs/tabs/tabs/default-tabs');
-        browser.setViewportSize(viewport);
-        browser.moveToObject('.tabContent');
-      });
-      Terra.it.matchesScreenshot();
-      Terra.it.isAccessible();
+Terra.describeViewports('Tabs - Responsive', ['tiny', 'small', 'medium', 'large', 'huge', 'enormous'], () => {
+  describe('Default', () => {
+    it('Default', () => {
+      browser.url('/#/raw/tests/terra-tabs/tabs/tabs/default-tabs');
+      browser.moveToObject('.tabContent');
+      Terra.validates.element();
     });
-    describe('Extended', () => {
-      before(() => {
-        browser.url('/#/raw/tests/terra-tabs/tabs/tabs/extended-tabs');
-        browser.setViewportSize(viewport);
-        browser.moveToObject('.tabContent');
-      });
-      Terra.it.matchesScreenshot();
-      Terra.it.isAccessible();
+  });
+  describe('Extended', () => {
+    it('Extended', () => {
+      browser.url('/#/raw/tests/terra-tabs/tabs/tabs/extended-tabs');
+      browser.moveToObject('.tabContent');
+      Terra.validates.element();
     });
-    describe('Icon Only Tabs', () => {
-      before(() => {
-        browser.url('/#/raw/tests/terra-tabs/tabs/tabs/icon-only-tabs');
-        browser.setViewportSize(viewport);
-        browser.moveToObject('.tabContent');
+  });
+  describe('Icon Only Tabs', () => {
+    it('Icon Only Tabs', () => {
+      browser.url('/#/raw/tests/terra-tabs/tabs/tabs/icon-only-tabs');
+      browser.moveToObject('.tabContent');
+      Terra.validates.element();
+    });
+  });
+});
+
+// Only test viewports that have collapsed tabs
+Terra.describeViewports('Responsive Hidden Open', ['tiny', 'small', 'medium', 'large'], () => {
+  before(() => {
+    browser.url('/#/raw/tests/terra-tabs/tabs/tabs/default-tabs');
+    browser.refresh();
+  });
+
+  it('should close menu when tab is selected', () => {
+    browser.waitForVisible('[data-terra-tabs-menu]');
+    browser.click('[data-terra-tabs-menu]');
+
+    Terra.validates.element('0', { rules: ignoredA11y, selector: '#root' });
+    browser.click('#tab12');
+
+    Terra.validates.element('1', { rules: ignoredA11y, selector: '#root' });
+  });
+});
+
+// Test desktop styles and functionality
+Terra.describeViewports('Tabs - Large screen', ['large'], () => {
+  describe('Default', () => {
+    it('Default', () => {
+      browser.url('/#/raw/tests/terra-tabs/tabs/tabs/default-tabs');
+      browser.refresh();
+      browser.moveToObject('.tabContent');
+    });
+
+    describe('Collapsible hover', () => {
+      it('Collapsible hover', () => {
+        browser.waitForVisible('#tab2');
+        browser.moveToObject('#tab2');
+        Terra.validates.element();
       });
-      Terra.it.matchesScreenshot();
-      Terra.it.isAccessible();
+    });
+
+    describe('Collapsible active focus', () => {
+      it('Collapsible active focus', () => {
+        browser.waitForVisible('#tab2');
+        browser.click('#tab2');
+        Terra.validates.element();
+      });
     });
   });
 
-  // Only test viewports that have collapsed tabs
-  Terra.viewports('tiny', 'small', 'medium', 'large').forEach((viewport) => {
-    describe('Responsive Hidden Open', () => {
-      before(() => {
-        browser.url('/#/raw/tests/terra-tabs/tabs/tabs/default-tabs');
-        browser.setViewportSize(viewport);
-        browser.click('[data-terra-tabs-menu]');
-      });
-
-      Terra.it.matchesScreenshot('0', { selector: '#root' });
-      Terra.it.isAccessible({ rules: ignoredA11y });
-
-      it('should close menu when tab is selected', () => {
-        browser.click('#tab12');
-      });
-
-      Terra.it.isAccessible({ rules: ignoredA11y });
-      Terra.it.matchesScreenshot('1');
-    });
-  });
-
-  // Test desktop styles and functionality
-  describe('Tabs - Large screen', () => {
-    before(() => browser.setViewportSize(Terra.viewports('large')[0]));
-    describe('Default', () => {
-      beforeEach(() => {
-        browser.url('/#/raw/tests/terra-tabs/tabs/tabs/default-tabs');
-        browser.moveToObject('.tabContent');
-      });
-
-      describe('Collapsible hover', () => {
-        beforeEach(() => {
-          browser.waitForVisible('#tab2');
-          browser.moveToObject('#tab2');
-        });
-
-        Terra.it.matchesScreenshot();
-        Terra.it.isAccessible();
-      });
-
-      describe('Collapsible active focus', () => {
-        beforeEach(() => {
-          browser.waitForVisible('#tab2');
-          browser.click('#tab2');
-        });
-
-        Terra.it.matchesScreenshot();
-        Terra.it.isAccessible();
-      });
-    });
-
-    describe('Fill Parent Tabs', () => {
-      beforeEach(() => {
-        browser.url('/#/raw/tests/terra-tabs/tabs/tabs/fill-parent-tabs');
-        browser.moveToObject('.tabContent');
-      });
-
-      Terra.it.matchesScreenshot();
-      Terra.it.isAccessible();
+  describe('Fill Parent Tabs', () => {
+    it('Fill Parent Tabs', () => {
+      browser.url('/#/raw/tests/terra-tabs/tabs/tabs/fill-parent-tabs');
+      browser.moveToObject('.tabContent');
+      Terra.validates.element();
     });
   });
 });
