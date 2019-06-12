6a5829b5401cbcb031978cb8b05a4b73acd13af3
Supreeth MR <supreeth.mr1990@gmail.com>
2019-06-12 12:11:46 +0530

    Corrected test file

--- packages/terra-collapsible-menu-view/src/terra-dev-site/test/collapsible-menu-view/DefaultCollapsibleMenuView.test.jsx
+++ packages/terra-collapsible-menu-view/src/terra-dev-site/test/collapsible-menu-view/DefaultCollapsibleMenuView.test.jsx
@@ -51,7 +51,6 @@ class CollapsibleMenuViewDemo extends React.Component {
           shouldCloseOnClick={false}
           onChange={this.handleToggleTwoOnChange}
           isSelected={this.state.toggle2Selection}
-          isSelectable
         />
         <CollapsibleMenuView.Divider key="Divider1" />
         <CollapsibleMenuView.Item
