f4e8b011368aedefbf07de34a063e55a24cb196d
Pranav Agarwal <Pranav.Agarwal@cerner.com>
2019-06-17 15:44:12 +0530

    removed extra line.

--- packages/terra-application-name/src/terra-dev-site/doc/example/ApplicationHeaderNameStandard.jsx
+++ packages/terra-application-name/src/terra-dev-site/doc/example/ApplicationHeaderNameStandard.jsx
@@ -24,7 +24,6 @@ const ApplicationHeaderNameStandard = () => (
     extensions={<Placeholder text="Extensions" type="header-extensions" />}
     navigation={<Placeholder text="Content" type="default" />}
     utilities={<Placeholder text="Utiltities" type="utiltities" />}
-
   />
 );
 
