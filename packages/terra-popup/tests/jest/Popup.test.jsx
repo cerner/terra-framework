04b88850a6b7d8d2eca77b4b0d003e677461a063
Emily Rohrbough <emilyrohrbough@users.noreply.github.com>
2019-06-18 16:40:11 -0500

    [terra-popup] Revamp Tests (#734)

--- packages/terra-popup/tests/jest/Popup.test.jsx
+++ packages/terra-popup/tests/jest/Popup.test.jsx
@@ -11,3 +11,18 @@ it('should shallow render a component with push attachment behavior', () => {
   const popup = shallow(<PopupDefaultExample attachmentBehavior="push" />);
   expect(popup).toMatchSnapshot();
 });
+
+it('should renders with custom arrow class name', () => {
+  const popup = shallow(<PopupDefaultExample classNameArrow="terra-test-class-arrow" />);
+  expect(popup).toMatchSnapshot();
+});
+
+it('should renders with custom content class name', () => {
+  const popup = shallow(<PopupDefaultExample classNameContent="terra-test-class-content" />);
+  expect(popup).toMatchSnapshot();
+});
+
+it('should renders with custom overlay class name', () => {
+  const popup = shallow(<PopupDefaultExample classNameOverlay="terra-test-class-overlay" />);
+  expect(popup).toMatchSnapshot();
+});
