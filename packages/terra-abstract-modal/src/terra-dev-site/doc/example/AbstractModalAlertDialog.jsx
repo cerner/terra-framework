689a443a8c606a6a9e18d8ee61c383f1246742d5
Brett Jankord <brett.jankord@cerner.com>
2019-07-19 11:40:54 -0500

    Push update to make alertdialog work with supported screen readers

--- packages/terra-abstract-modal/src/terra-dev-site/doc/example/AbstractModalAlertDialog.jsx
+++ packages/terra-abstract-modal/src/terra-dev-site/doc/example/AbstractModalAlertDialog.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
 // eslint-disable-next-line import/no-extraneous-dependencies, import/no-unresolved, import/extensions
 import AbstractModal from 'terra-abstract-modal/lib/AbstractModal';
 import classNames from 'classnames/bind';
@@ -7,7 +7,36 @@ import generalStyles from './AbstractModalDocCommon.module.scss';
 
 const cx = classNames.bind(generalStyles);
 
-class AbstractModalIsOpen extends React.Component {
+/* eslint-disable-next-line react/prop-types */
+const AlertDialogContent = ({ handleCloseModal }) => {
+  const confirmBtn = useRef(null);
+
+  useEffect(() => {
+    confirmBtn.current.focus();
+  }, []);
+
+  return (
+    <div id="alert-dialog-content" className={cx('content-wrapper')}>
+      <h1>Alert Dialog Modal</h1>
+      <br />
+      <p>
+        The abstract modal can be used to create an alert dialog modal. You can use the role,
+        {' '}
+        <code>alertdialog</code>
+        {' '}
+        , to create a modal dialog that interrupts the users workflow to get a response, usually some sort of confirmation.
+      </p>
+      <button ref={confirmBtn} type="button" onClick={handleCloseModal}>Confirm action</button>
+      <button type="button" onClick={handleCloseModal}>Close Modal</button>
+    </div>
+  );
+};
+
+/* VoiceOver will not read the content of the modal dialog if the role attribute
+ is set to `alertdialog` and ariaLabel is defined */
+const ariaLabel = '';
+
+class AbstractModalAlertDialog extends React.Component {
   constructor() {
     super();
 
@@ -31,30 +60,15 @@ class AbstractModalIsOpen extends React.Component {
     return (
       <div>
         <AbstractModal
+          ariaLabel={ariaLabel}
+          aria-labelledby="alert-dialog-content"
           isOpen={this.state.isOpen}
           onRequestClose={this.handleCloseModal}
           closeOnOutsideClick={false}
           classNameModal={styles['fixed-size']}
           role="alertdialog"
         >
-          <div className={cx('content-wrapper')}>
-            <h1>Alert Dialog Modal</h1>
-            <br />
-            <p>
-              The abstract modal can be used to create an alert dialog modal. You can use the role,
-              {' '}
-              <code>alertdialog</code>
-              {' '}
-              , to create a modal dialog that interrupts the users workflow to get a response, usually some sort of confirmation. This is different than a typical modal dialog in that it requires some user response, like &quot;Save&quot;, or &quot;Cancel&quot;, etc.
-            </p>
-            <p>You can close the modal by:</p>
-            <ul>
-              <li>- Pressing the ESC key</li>
-              <li>- Clicking on the close button</li>
-            </ul>
-            <br />
-            <button type="button" onClick={this.handleCloseModal}>Close Modal</button>
-          </div>
+          <AlertDialogContent handleCloseModal={this.handleCloseModal} />
         </AbstractModal>
         <button type="button" onClick={this.handleOpenModal}>Open Modal</button>
       </div>
@@ -62,4 +76,4 @@ class AbstractModalIsOpen extends React.Component {
   }
 }
 
-export default AbstractModalIsOpen;
+export default AbstractModalAlertDialog;
