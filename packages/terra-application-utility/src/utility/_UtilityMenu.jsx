1909155b96664c1129dbf8e3ed102f91f11b16ca
Brett Jankord <brett.jankord@cerner.com>
2019-07-19 11:43:27 -0500

    Add aria-level

--- packages/terra-application-utility/src/utility/_UtilityMenu.jsx
+++ packages/terra-application-utility/src/utility/_UtilityMenu.jsx
@@ -339,7 +339,7 @@ class UtilityMenu extends React.Component {
         <span className={contentContainerClassNames}>
           <span className={leftContentContainer}>
             {!firstPage && backButton}
-            <span id="utility-menu-header" role="heading" className={headerTextClassName}>{headerText}</span>
+            <span id="utility-menu-header" role="heading" aria-level="2" className={headerTextClassName}>{headerText}</span>
           </span>
           <span className={cx('utility-menu-right-content-container')}>
             {closeButton}
