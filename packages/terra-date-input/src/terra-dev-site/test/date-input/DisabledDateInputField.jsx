3ef20c2e4204fdf920381896c8ec433f71d6f886
Brett Jankord <brett.jankord@cerner.com>
2019-08-20 12:18:07 -0500

    Add disabled example test files

---
+++ packages/terra-date-input/src/terra-dev-site/test/date-input/DisabledDateInputField.jsx
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+
+// eslint-disable-next-line import/no-extraneous-dependencies, import/no-unresolved, import/extensions
+import DateInputField from 'terra-date-input/lib/DateInputField';
+
+const Example = () => {
+  const [value, setValue] = useState('');
+  const [isInvalid, setIsInvalid] = useState(false);
+
+  return (
+    <div>
+      <DateInputField
+        legend="Legend text"
+        name="date-input"
+        value={value}
+        onChange={(event, dateString) => setValue(dateString)}
+        error="Error message"
+        help="Help message"
+        isInvalid={isInvalid}
+        disabled
+      />
+      <p>{`DateInputField Value: ${value}`}</p>
+      <button type="button" onClick={() => setIsInvalid(invalid => !invalid)}>Toggle isInvalid</button>
+    </div>
+  );
+};
+
+export default Example;
