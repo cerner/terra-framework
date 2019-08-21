3a9a07f420a3619fa9803adeda89559ccb06ba33
Brett Jankord <brett.jankord@cerner.com>
2019-08-20 20:29:10 -0500

    Start work on jest tests

--- packages/terra-date-input/tests/jest/DateInput.test.jsx
+++ packages/terra-date-input/tests/jest/DateInput.test.jsx
@@ -1,24 +1,27 @@
 import React from 'react';
+/* eslint-disable-next-line import/no-extraneous-dependencies */
+import { shallowWithIntl } from 'terra-enzyme-intl';
+
 import DateInput from '../../src/DateInput';
 
 describe('DateInput', () => {
-  const defaultRender = <DateInput />;
+  const defaultRender = <DateInput name="date-input" />;
 
   // Snapshot Tests
   it('should render a default component', () => {
-    const wrapper = shallow(defaultRender);
+    const wrapper = shallowWithIntl(defaultRender);
     expect(wrapper).toMatchSnapshot();
   });
 
   // Prop Tests
-  it('should use the default value when no value is given', () => {
-    const wrapper = shallow(defaultRender);
-    expect(wrapper.find('.date-input').text()).toEqual('default');
-  });
+  // it('should use the default value when no value is given', () => {
+  //   const wrapper = shallow(defaultRender);
+  //   expect(wrapper.find('.date-input').text()).toEqual('default');
+  // });
 
   // Structure Tests
-  it('should have the class date-input', () => {
-    const wrapper = shallow(defaultRender);
-    expect(wrapper.prop('className')).toContain('date-input');
-  });
+  // it('should have the class date-input', () => {
+  //   const wrapper = shallow(defaultRender);
+  //   expect(wrapper.prop('className')).toContain('date-input');
+  // });
 });
