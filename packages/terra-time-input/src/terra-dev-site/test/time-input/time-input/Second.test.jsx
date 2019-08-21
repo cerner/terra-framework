8de1b1df8c62321460a48f5374f1caa2c8ab715c
Ryan Manuel <rfmanuel@gmail.com>
2019-08-21 17:04:20 -0500

    Update Second.test.jsx

--- packages/terra-time-input/src/terra-dev-site/test/time-input/time-input/Second.test.jsx
+++ packages/terra-time-input/src/terra-dev-site/test/time-input/time-input/Second.test.jsx
@@ -2,8 +2,6 @@ import React from 'react';
 import classNames from 'classnames/bind';
 import TimeInput from '../../../../TimeInput';
 
-const cx = classNames.bind(styles);
-
 const TimeInputDefault = () => (
   <TimeInput
     id="timeInput"
