2fb5ed0fa5a6ea073c8d6b8fd204d6b57e7fa93f
Tyler Biethman <tbiethman@gmail.com>
2019-09-17 22:41:45 -0500

    Updating example with NavigationPrompt

--- packages/terra-application/src/terra-dev-site/doc/example/ApplicationBaseExample.jsx
+++ packages/terra-application/src/terra-dev-site/doc/example/ApplicationBaseExample.jsx
@@ -3,10 +3,12 @@ import { injectIntl } from 'react-intl';
 import { ActiveBreakpointContext } from '../../../breakpoints';
 import ApplicationLoadingOverlay from '../../../application-loading-overlay';
 import ApplicationBase from '../../../application-base';
+import NavigationPrompt from '../../../navigation-prompt';
 
 const ApplicationContentExample = injectIntl(({ intl }) => {
   const [isLoading, setIsLoading] = useState(false);
   const [throwError, setThrowError] = useState(false);
+  const [blockUnload, setBlockUnload] = useState(false);
 
   const activeBreakpoint = useContext(ActiveBreakpointContext);
 
@@ -14,29 +16,65 @@ const ApplicationContentExample = injectIntl(({ intl }) => {
     throw new Error("Testing ApplicationBase's error boundary...");
   }
 
-  return (
-    <div>
-      <p>
+  const activeBreakpointExample = (
+    <p>
         Active Breakpoint:
-        {' '}
-        {activeBreakpoint}
-      </p>
-      <p>
-        Active Locale:
-        {' '}
-        {intl.locale}
-      </p>
-      <p>
-        Render Error:
-        {' '}
-        <button type="button" onClick={() => { setThrowError(true); }}>Throw</button>
-      </p>
+      {' '}
+      {activeBreakpoint}
+    </p>
+  );
+
+  const intlExample = (
+    <p>
+    Active Locale:
+      {' '}
+      {intl.locale}
+    </p>
+  );
+
+  const errorBoundaryExample = (
+    <p>
+    Render Error:
+      {' '}
+      <button type="button" onClick={() => { setThrowError(true); }}>Throw</button>
+    </p>
+  );
+
+  const loadingOverlayExample = (
+    <>
       <p>
         Show Loading Overlay (3 seconds):
         {' '}
         <button type="button" onClick={() => { setIsLoading(true); setTimeout(() => { setIsLoading(false); }, 3000); }}>Show</button>
       </p>
       <ApplicationLoadingOverlay isOpen={isLoading} message="Testing ApplicationBase's loading overlay" />
+    </>
+  );
+
+  const navigationPromptExample = (
+    <>
+      <p>
+        Prompt on window close:
+        {' '}
+        <input
+          type="checkbox"
+          name="scales"
+          onChange={() => {
+            setBlockUnload(!blockUnload);
+          }}
+        />
+      </p>
+      {blockUnload ? <NavigationPrompt description="Testing ApplicationBase's navigation prompt handling" /> : undefined}
+    </>
+  );
+
+  return (
+    <div>
+      {activeBreakpointExample}
+      {intlExample}
+      {errorBoundaryExample}
+      {loadingOverlayExample}
+      {navigationPromptExample}
     </div>
   );
 });
