194d0eb039b463d5afd7e3eeeeb47dbd31188850
BenBoersma <Ben.Boersma@cerner.com>
2019-09-20 14:09:55 -0500

    Changing content example

--- packages/terra-notification-dialog/src/terra-dev-site/doc/example/ContentNotificationDialog.jsx
+++ packages/terra-notification-dialog/src/terra-dev-site/doc/example/ContentNotificationDialog.jsx
@@ -34,7 +34,12 @@ class ContentNotificationDialog extends React.Component {
           isOpen={this.state.isOpen}
           title="Make sure that the title relates directly to the choices."
           startMessage="The Main Instruction is text used to provide more detail or define terminology. Don’t repeat the title verbatim."
-          content={<Button text="Do a thing" />}
+          content={(
+            <ul>
+              <li>Item 1</li>
+              <li>Item 2</li>
+            </ul>
+          )}
           endMessage="The Main Instruction is text used to provide more detail or define terminology. Don’t repeat the title verbatim."
           acceptAction={{
             text: 'Confirm',
