1787ea968878c8d99067d88b360d8098f58b8181
Cai,Ben <BCAI@CERNER.COM>
2019-09-26 16:37:41 -0500

    Update prop description.

--- packages/terra-time-input/src/TimeInput.jsx
+++ packages/terra-time-input/src/TimeInput.jsx
@@ -59,7 +59,7 @@ const propTypes = {
    */
   refCallback: PropTypes.func,
   /**
-   * Custom input attribues to apply to the seconds input
+   * Custom input attributes to apply to the seconds input
    */
   // eslint-disable-next-line react/forbid-prop-types
   secondAttributes: PropTypes.object,
@@ -73,7 +73,9 @@ const propTypes = {
    */
   value: PropTypes.string,
   /**
-   * Type of time input to initialize. Must be '24-hour' or '12-hour'
+   * Type of time input to initialize. Must be `24-hour` or `12-hour`. 
+   * The `de`, `es-ES`, `fr-FR`, `fr`, `nl-BE`, `nl`, `pt-BR`, `pt`, `sv-SE` and `sv` locales do not use the 12-hour time notation.
+   * If the `variant` prop if set to `12-hour` for one of these supported locales, the variant will be ignored and defaults to `24-hour`.
    */
   variant: PropTypes.oneOf([TimeUtil.FORMAT_12_HOUR, TimeUtil.FORMAT_24_HOUR]),
 };
