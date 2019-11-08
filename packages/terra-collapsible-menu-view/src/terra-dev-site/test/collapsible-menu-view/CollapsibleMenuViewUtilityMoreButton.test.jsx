6cf7f8722f57836915c06589297b30efd09fb1df
Naveen Kumar Ramamurthy <nramamurth@users.noreply.github.com>
2019-11-08 09:50:54 +0530

    [terra-collapsible-menu-view] Add ability for 'More Button' to switch variants. (#943)
    * [terra-collapsible-menu-view] Add ability for 'More Button' to switch variants.
    
    * Rename prop.

---
+++ packages/terra-collapsible-menu-view/src/terra-dev-site/test/collapsible-menu-view/CollapsibleMenuViewUtilityMoreButton.test.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import IconSend from 'terra-icon/lib/icon/IconSend';
+import CollapsibleMenuView from '../../../CollapsibleMenuView';
+
+const CollapsibleMenuViewDemo = () => (
+  <CollapsibleMenuView menuButtonVariant="utility">
+    <CollapsibleMenuView.Item text="Send Notification 1" className="ButtonA" key="buttonA" />
+    <CollapsibleMenuView.Item text="Send Notification 2" className="ButtonB" key="buttonB" />
+    <CollapsibleMenuView.Divider key="Divider4" />
+    <CollapsibleMenuView.Item text="Button 1" className="Button1" key="button1" />
+    <CollapsibleMenuView.Item text="Button 2" className="Button2" key="button2" />
+    <CollapsibleMenuView.Item
+      icon={<IconSend />}
+      text="Button3"
+      className="IconOnly"
+      key="button3"
+      isIconOnly
+    />
+    <CollapsibleMenuView.Item text="Button C" className="ButtonC" key="buttonC" />
+    <CollapsibleMenuView.Item text="Button D" className="ButtonD" key="buttonD" />
+    <CollapsibleMenuView.Divider key="Divider5" />
+    <CollapsibleMenuView.Item text="Button E" className="ButtonE" key="buttonE" />
+    <CollapsibleMenuView.Item text="Button F" className="ButtonF" key="buttonF" />
+    <CollapsibleMenuView.Item text="Button G" className="ButtonG" key="buttonG" />
+    <CollapsibleMenuView.Item text="Button H" className="ButtonH" key="buttonH" />
+    <CollapsibleMenuView.Divider key="Divider6" />
+    <CollapsibleMenuView.Item text="Send Notification to Voice Assistant" className="ButtonI" key="buttonI" />
+    <CollapsibleMenuView.Item text="Send Notification to Creative Director Tomorrow" className="ButtonJ" key="buttonJ" />
+  </CollapsibleMenuView>
+);
+
+export default CollapsibleMenuViewDemo;
