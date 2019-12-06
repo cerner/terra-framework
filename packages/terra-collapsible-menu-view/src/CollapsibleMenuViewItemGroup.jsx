cb4017380a3b1a2090317e2f221f9ce29bac4cf7
saket2403 <saket.bajaj1998@gmail.com>
2019-12-06 22:16:31 +0530

    custom props

--- packages/terra-collapsible-menu-view/src/CollapsibleMenuViewItemGroup.jsx
+++ packages/terra-collapsible-menu-view/src/CollapsibleMenuViewItemGroup.jsx
@@ -77,7 +77,7 @@ class CollapsibleMenuViewItemGroup extends React.Component {
       );
     } if (isCollapsibleMenuItem) {
       return (
-        <div>
+        <div {...customProps}>
           { children }
         </div>
       );
