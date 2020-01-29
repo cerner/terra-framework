a5d76f24bbf598f9b8c838dcefffb28ec2098b4b
Emily Rohrbough <emily.rohrbough@cerner.com>
2020-01-29 14:00:55 -0600

    let see if this speeds up travis a bit

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,12 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 const terraDevSiteWebpackConfig = require('terra-dev-site/config/webpack/webpack.config');
 
-module.exports = terraDevSiteWebpackConfig;
+module.exports = (env, argv) => {
+  const aggregateOptions = {};
+
+  if (process.env.LOCALE) {
+    aggregateOptions.locales = [process.env.LOCALE];
+  }
+
+  return terraDevSiteWebpackConfig({ ...env, aggregateOptions }, argv);
+};
