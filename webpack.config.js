385252f8858cb9a5e0dc0eabc78c469b56674b2b
Adam Parker <amichaelparker@gmail.com>
2020-01-30 14:52:19 -0600

    screenshot updates

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,9 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
+const merge = require('webpack-merge');
 const terraDevSiteWebpackConfig = require('terra-dev-site/config/webpack/webpack.config');
 
-module.exports = terraDevSiteWebpackConfig;
+const mergedConfig = (env, argv) => (
+  merge(terraDevSiteWebpackConfig(env, argv), { devServer: { clientLogLevel: 'debug', stats: { assets: false } }, watchOptions: { ignored: /aggregated-translations/ } })
+);
+
+module.exports = mergedConfig;
