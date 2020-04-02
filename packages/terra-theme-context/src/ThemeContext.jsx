33591bb3d73d66d39c38cc420f1bf07897100ab9
Matt Henkes <mjhenkes@gmail.com>
2020-04-02 08:36:21 -0500

    my own review updates

--- packages/terra-theme-context/src/ThemeContext.jsx
+++ packages/terra-theme-context/src/ThemeContext.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
-/**
- * Creates an app settings context to share global app setting with the dev toolbar.
- */
+
 const ThemeProviderContext = React.createContext({});
 
 export default ThemeProviderContext;
