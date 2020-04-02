8398f7e62234b6c44502add564933a56987e3a40
Matt Henkes <mjhenkes@gmail.com>
2020-04-02 14:17:05 -0500

    Memoizing, documentation updates

--- packages/terra-theme-context/src/ThemeContextProvider.jsx
+++ packages/terra-theme-context/src/ThemeContextProvider.jsx
@@ -1,17 +1,26 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import PropTypes from 'prop-types';
 import ThemeContext from './ThemeContext';
 
 const propTypes = {
+  /**
+   * The components to be rendered within the context of the ThemeContextProvider. Components rendered here are able to interact with ThemeContextProvider through the ThemeContext.
+   */
   children: PropTypes.element.isRequired,
+  /**
+   * The currently selected application theme to be provided via the ThemeContext.
+   */
   theme: PropTypes.string,
 };
 
-const ThemeContextProvider = ({ theme, children }) => (
-  <ThemeContext.Provider value={{ theme }}>
-    {children}
-  </ThemeContext.Provider>
-);
+const ThemeContextProvider = ({ theme, children }) => {
+  const contextValue = useMemo(() => ({ theme }), [theme]);
+  return (
+    <ThemeContext.Provider value={contextValue}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
 
 ThemeContextProvider.propTypes = propTypes;
 
