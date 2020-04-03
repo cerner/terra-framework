fa8a8b141b36993ea002ffea46f34f3d29a2a795
Matt Henkes <mjhenkes@gmail.com>
2020-04-03 10:26:35 -0500

    Adding context shape.

--- packages/terra-theme-context/src/ThemeContext.jsx
+++ packages/terra-theme-context/src/ThemeContext.jsx
@@ -1,5 +1,15 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const ThemeProviderContext = React.createContext({});
 
+const contextShape = PropTypes.shape({
+  /**
+   * The current application theme className.
+   * The default theme is indicated as undefined.
+   */
+  className: PropTypes.string,
+});
+
 export default ThemeProviderContext;
+export { contextShape };
