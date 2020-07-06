d7471caff9a34cc7d9839a8e6199f10292ca9773
P <lokesh.p@cerner.com>
2020-07-06 20:55:57 +0530

    update doc and examples

--- packages/terra-slide-panel-manager/src/SlidePanelManager.jsx
+++ packages/terra-slide-panel-manager/src/SlidePanelManager.jsx
@@ -77,7 +77,7 @@ class SlidePanelManager extends React.Component {
       panelSize = disclosureSizeToPanelSize[manager.disclosure.size];
     }
 
-    const panelBehavior = manager.disclosure.typeConfig.panelBehavior ? manager.disclosure.typeConfig.panelBehavior : this.props.panelBehavior;
+    const panelBehavior = manager.disclosure.typeConfig && manager.disclosure.typeConfig.panelBehavior ? manager.disclosure.typeConfig.panelBehavior : this.props.panelBehavior;
 
     const presentedDisclosureComponentKey = manager.disclosureComponentKeys[manager.disclosureComponentKeys.length - 1];
     const presentedDisclosureComponentData = manager.disclosureComponentData[presentedDisclosureComponentKey] || {};
@@ -109,7 +109,7 @@ class SlidePanelManager extends React.Component {
                 ) : undefined}
                 {disclosureAccessory}
               </React.Fragment>
-            )}
+          )}
           >
             <SlideGroup items={manager.disclosure.components} isAnimated />
           </ContentContainer>
