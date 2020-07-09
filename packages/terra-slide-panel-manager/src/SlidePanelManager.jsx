ce609cca018e1a3729cc0fcf73b7e8c49cbb1110
P <lokesh.p@cerner.com>
2020-07-09 19:20:04 +0530

    update condition check

--- packages/terra-slide-panel-manager/src/SlidePanelManager.jsx
+++ packages/terra-slide-panel-manager/src/SlidePanelManager.jsx
@@ -77,7 +77,9 @@ class SlidePanelManager extends React.Component {
       panelSize = disclosureSizeToPanelSize[manager.disclosure.size];
     }
 
-    const panelBehavior = manager.disclosure.typeConfig && manager.disclosure.typeConfig.panelBehavior ? manager.disclosure.typeConfig.panelBehavior : customProps.panelBehavior;
+    const panelBehavior = manager?.disclosure?.typeConfig?.panelBehavior ? manager.disclosure.typeConfig.panelBehavior : customProps.panelBehavior;
+
+    console.log("panel",panelBehavior);
 
     const presentedDisclosureComponentKey = manager.disclosureComponentKeys[manager.disclosureComponentKeys.length - 1];
     const presentedDisclosureComponentData = manager.disclosureComponentData[presentedDisclosureComponentKey] || {};
