d885051da0c2b11e63e291564f9e9377ce62609b
P <lokesh.p@cerner.com>
2020-07-12 23:16:27 +0530

    pilot

--- packages/terra-application-navigation/src/tabs/_Tabs.jsx
+++ packages/terra-application-navigation/src/tabs/_Tabs.jsx
@@ -60,7 +60,7 @@ class Tabs extends React.Component {
     this.handleResize = this.handleResize.bind(this);
     this.renderRollup = this.renderRollup.bind(this);
     this.renderPopup = this.renderPopup.bind(this);
-    this.buildVisibleChildren = this.buildVisibleChildren.bind(this);
+    this.renderVisibleTabs = this.renderVisibleTabs.bind(this);
     this.updateSize = LodashDebounce(this.updateSize.bind(this), 100);
     this.initializeResize = this.initializeResize.bind(this);
     this.removeResize = this.removeResize.bind(this);
@@ -204,25 +204,6 @@ class Tabs extends React.Component {
     return shouldPulse;
   }
 
-  buildVisibleChildren(visibleTabs, hasNotifications, onTabSelect, activeTabKey, notifications) {
-    return visibleTabs.map((tab, index) => {
-      const tabProps = {
-        text: tab.text,
-        key: tab.key,
-        onTabSelect: onTabSelect ? onTabSelect.bind(null, tab.key, tab.metaData) : null,
-        isActive: tab.key === activeTabKey,
-        notificationCount: hasNotifications ? notifications[tab.key] : 0,
-        hasCount: hasNotifications,
-      };
-      if (this.isCalculating) {
-        const tabRef = React.createRef();
-        this.childRefs[index] = tabRef;
-        tabProps.tabRef = tabRef;
-      }
-      return <Tab {...tabProps} render={this.props.navigationRenderFunction} />;
-    });
-  }
-
   sliceTabs(navigationItems) {
     if (this.hiddenStartIndex >= 0) {
       return {
@@ -236,15 +217,37 @@ class Tabs extends React.Component {
     };
   }
 
-  renderRollup(hiddenTabs, hasNotifications, hasHiddenNotification) {
-    const { activeTabKey, notifications, intl } = this.props;
+  renderVisibleTabs(visibleTabs, useNotificationStyle, notifications) {
+    const { activeTabKey, onTabSelect } = this.props;
+
+    return visibleTabs.map((tab, index) => {
+      const tabProps = {
+        text: tab.text,
+        key: tab.key,
+        onTabSelect: onTabSelect ? onTabSelect.bind(null, tab.key, tab.metaData) : null,
+        isActive: tab.key === activeTabKey,
+        notificationCount: useNotificationStyle ? notifications[tab.key] : 0,
+        hasCount: useNotificationStyle,
+      };
+      if (this.isCalculating) {
+        const tabRef = React.createRef();
+        this.childRefs[index] = tabRef;
+        tabProps.tabRef = tabRef;
+      }
+      return <Tab {...tabProps} render={this.props.navigationRenderFunction} />;
+    });
+  }
+
+  renderRollup(hiddenTabs, useNotificationStyle, notifications) {
+    const { activeTabKey, intl } = this.props;
 
     const tabRollupIsSelected = hiddenTabs.some(tab => tab.key === activeTabKey);
+    const hasChildNotifications = hiddenTabs.some(tab => notifications[tab.key] > 0);
 
     return (
       <TabRollup
-        hasCount={hasNotifications}
-        isPulsed={hasHiddenNotification && !this.isCalculating && this.shouldPulse(hiddenTabs, notifications)}
+        hasCount={useNotificationStyle}
+        isPulsed={hasChildNotifications && !this.isCalculating && this.shouldPulse(hiddenTabs, notifications)}
         onTabSelect={() => {
           this.setState({ popupIsOpen: true });
         }}
@@ -252,15 +255,15 @@ class Tabs extends React.Component {
         innerRef={this.rollupInnerRef}
         text={intl.formatMessage({ id: 'Terra.applicationNavigation.tabs.rollupButtonTitle' })}
         isSelected={tabRollupIsSelected}
-        hasChildNotifications={hasHiddenNotification}
+        hasChildNotifications={hasChildNotifications}
         data-application-tabs-more
       />
     );
   }
 
-  renderPopup(hiddenTabs) {
+  renderPopup(hiddenTabs, notifications) {
     const {
-      activeTabKey, onTabSelect, notifications, intl,
+      activeTabKey, onTabSelect, intl,
     } = this.props;
 
     return (
@@ -302,8 +305,6 @@ class Tabs extends React.Component {
   render() {
     const {
       navigationItems,
-      activeTabKey,
-      onTabSelect,
       notifications,
     } = this.props;
     if (!navigationItems || !navigationItems.length) {
@@ -312,18 +313,16 @@ class Tabs extends React.Component {
 
     const { popupIsOpen } = this.state;
     const { visibleTabs, hiddenTabs } = this.sliceTabs(navigationItems);
-    const hasVisibleNotification = visibleTabs.some(tab => !!notifications[tab.key]);
-    const hasHiddenNotification = hiddenTabs.some(tab => !!notifications[tab.key]);
-    const hasNotifications = hasVisibleNotification || hasHiddenNotification;
+    const useNotificationStyle = !!Object.keys(notifications).length;
 
     return (
       <nav
         className={cx('tabs-container', { 'is-calculating': this.isCalculating })}
         ref={this.containerRef}
       >
-        {this.buildVisibleChildren(visibleTabs, hasNotifications, onTabSelect, activeTabKey, notifications)}
-        {!this.menuHidden ? this.renderRollup(hiddenTabs, hasNotifications, hasHiddenNotification) : null}
-        {popupIsOpen ? this.renderPopup(hiddenTabs) : null}
+        {this.renderVisibleTabs(visibleTabs, useNotificationStyle, notifications)}
+        {!this.menuHidden ? this.renderRollup(hiddenTabs, useNotificationStyle, notifications) : null}
+        {popupIsOpen ? this.renderPopup(hiddenTabs, notifications) : null}
       </nav>
     );
   }
