28381ebca90ab81ea47c29b0082448205d0b2c6f
lokesh-0813 <lokesh.0813@gmail.com>
2020-09-02 00:04:27 +0530

    update devsite example

---
+++ packages/terra-date-time-picker/src/terra-dev-site/doc/example/DateTimePickerTimeZone.jsx
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+import DateTimePicker from 'terra-date-time-picker/lib/DateTimePicker';
+import moment from 'moment';
+import Field from 'terra-form-field';
+import DateTimeUtils from '../../../DateTimeUtils';
+
+function DateTimePickerTimeZone() {
+  const [timeZone, setTimezone] = useState('Asia/Kolkata');
+  const [dateTime, setDateTime] = useState(moment().format());
+
+  const handleDateTimeChange = (event, datetime) => {
+    const computedDateTime = DateTimeUtils.createSafeDate(datetime, timeZone);
+
+    setDateTime(computedDateTime.format());
+    setTimezone(computedDateTime.tz());
+  };
+
+  return (
+    <>
+      <p>
+        Selected ISO Date Time:
+        {dateTime}
+      </p>
+      <p>
+        Selected Timezone:
+        {timeZone}
+      </p>
+      <Field label="Enter Date/Time" htmlFor="timeZone">
+        <DateTimePicker
+          name="date-time-picker-example"
+          dateInputAttributes={{ id: 'timezone' }}
+          onChange={handleDateTimeChange}
+          initialTimeZone={timeZone}
+          value={dateTime}
+        />
+      </Field>
+    </>
+  );
+}
+
+export default DateTimePickerTimeZone;
