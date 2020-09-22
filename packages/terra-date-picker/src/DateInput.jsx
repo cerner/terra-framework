a432c16a86d3c878577630ed2ee78bdf83021d56
pranav agarwal <agarwal.pranav300@gmail.com>
2020-09-22 19:04:41 +0530

    minor changes

--- packages/terra-date-picker/src/DateInput.jsx
+++ packages/terra-date-picker/src/DateInput.jsx
@@ -195,11 +195,11 @@ class DatePickerInput extends React.Component {
 
     const label = this.props.ariaLabel ? this.props.ariaLabel : intl.formatMessage({ id: 'Terra.datePicker.date' });
 
-    if (useExternalFormatMask === false && inputAttributes['aria-describedby']) {
+    if (useExternalFormatMask === false && inputAttributes && inputAttributes['aria-describedby']) {
       this.formatDescriptionId = `terra-date-picker-description-format-${this.uuid} ${inputAttributes['aria-describedby']}`;
     } else if (useExternalFormatMask === false) {
       this.formatDescriptionId = `terra-date-picker-description-format-${this.uuid}`;
-    } else if (inputAttributes['aria-describedby']) {
+    } else if (inputAttributes && inputAttributes['aria-describedby']) {
       this.formatDescriptionId = inputAttributes['aria-describedby'];
     } else {
       this.formatDescriptionId = '';
