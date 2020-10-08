79579c777de14958b0841fd86ea4ed23f223b0a6
Saket Bajaj <42207428+saket2403@users.noreply.github.com>
2020-10-08 10:26:27 +0530

    Suggestion
    Co-authored-by: Derek Yu <yuderekyu@gmail.com>

--- packages/terra-abstract-modal/src/AbstractModal.jsx
+++ packages/terra-abstract-modal/src/AbstractModal.jsx
@@ -118,7 +118,8 @@ const AbstractModal = (props) => {
 
   const handleKeydown = useCallback(e => {
     if (e.keyCode === KEY_ESCAPE && closeOnEsc && isOpen) {
-      if (modalElementRef.current) {
+      const currentModalNode = modalElementRef.current;
+      if (currentModalNode && (e.target === currentModalNode|| currentModalNode.contains(e.target))) {
         if (e.target === modalElementRef.current || modalElementRef.current.contains(e.target)) {
           onRequestClose();
         }
