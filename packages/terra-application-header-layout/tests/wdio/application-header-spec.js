23cb86f18821928d30c913ed4b29a6f4e50958a0
saket2403 <saket.bajaj1998@gmail.com>
2020-11-10 14:43:50 +0530

    Remove it helpers

--- packages/terra-application-header-layout/tests/wdio/application-header-spec.js
+++ packages/terra-application-header-layout/tests/wdio/application-header-spec.js
@@ -1,11 +1,8 @@
 Terra.describeViewports('Application Header Layout', ['tiny', 'medium'], () => {
-  describe('Displays a default application header layout', () => {
-    before(() => {
-      browser.url('/#/raw/tests/terra-application-header-layout/application-header-layout/application-header-default');
-      browser.waitForVisible('#test-header');
-    });
-
-    Terra.it.matchesScreenshot({ selector: '#test-header' });
+  it('Displays a default application header layout', () => {
+    browser.url('/#/raw/tests/terra-application-header-layout/application-header-layout/application-header-default');
+    browser.waitForVisible('#test-header');
+    Terra.validates.screenshot({ selector: '#test-header' });
   });
 
   describe('Displays the Skip to Content when focused onto', () => {
@@ -15,21 +12,19 @@ Terra.describeViewports('Application Header Layout', ['tiny', 'medium'], () => {
       browser.keys(['Tab']);
     });
 
-    Terra.it.validatesElement('Visible', { selector: '#test-header' });
+    it('should be visible', () => {
+      Terra.validates.element('Visible', { selector: '#test-header' });
+    });
 
     it('should hide the button after a tab', () => {
       browser.keys(['Tab', 'Tab']);
+      Terra.validates.element('Not Visible', { selector: '#test-header' });
     });
-
-    Terra.it.validatesElement('Not Visible', { selector: '#test-header' });
   });
 
-  describe('Displays a application header layout with logo and utilities only', () => {
-    before(() => {
-      browser.url('/#/raw/tests/terra-application-header-layout/application-header-layout/application-header-with-logo-and-utilities-only');
-      browser.waitForVisible('#test-header');
-    });
-
-    Terra.it.validatesElement({ selector: '#test-header' });
+  it('Displays a application header layout with logo and utilities only', () => {
+    browser.url('/#/raw/tests/terra-application-header-layout/application-header-layout/application-header-with-logo-and-utilities-only');
+    browser.waitForVisible('#test-header');
+    Terra.validates.element('with logo and utilities only', { selector: '#test-header' });
   });
 });
