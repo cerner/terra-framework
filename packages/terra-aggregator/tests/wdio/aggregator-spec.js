0ee90a69bcb2571267e9d063806390ce6e30d2a7
Saket Bajaj <42207428+saket2403@users.noreply.github.com>
2020-11-12 23:00:19 +0530

    [terra-aggregator] Remove Terra.it helpers from wdio  (#1303)
    * Remove it helpers
    
    * Fix url
    
    Co-authored-by: Matt Henkes <mjhenkes@gmail.com>

--- packages/terra-aggregator/tests/wdio/aggregator-spec.js
+++ packages/terra-aggregator/tests/wdio/aggregator-spec.js
@@ -1,54 +1,36 @@
 Terra.describeViewports('Aggregator', ['large'], () => {
-  describe('Standalone', () => {
-    before(() => {
-      browser.url('/#/raw/tests/terra-aggregator/aggregator/aggregator-standalone');
-
-      browser.click('#test-aggregator #section1');
-      browser.click('#test-aggregator #section2');
-      browser.click('#test-aggregator #section3');
-    });
-
-    Terra.it.validatesElement();
+  it('displays standalone aggregator', () => {
+    browser.url('/raw/tests/terra-aggregator/aggregator/aggregator-standalone');
+    browser.click('#test-aggregator #section1');
+    browser.click('#test-aggregator #section2');
+    browser.click('#test-aggregator #section3');
+    Terra.validates.element('Standalone');
   });
-
-  describe('With Disclosure - Open', () => {
-    before(() => {
-      browser.url('/#/raw/tests/terra-aggregator/aggregator/aggregator-with-disclosure');
+  describe('With Disclosure', () => {
+    it('displays aggregator with disclosure - open', () => {
+      browser.url('/raw/tests/terra-aggregator/aggregator/aggregator-with-disclosure');
       browser.click('#test-aggregator #section1');
+      Terra.validates.element('Open', { selector: '#test-aggregator' });
     });
 
-    Terra.it.validatesElement({ selector: '#test-aggregator' });
-
-    describe('With Disclosure - Close from focus release', () => {
-      before(() => {
-        browser.waitForVisible('[class*="slide-group"] .close-disclosure');
-
-        browser.click('#test-aggregator #section1');
-      });
-
-      Terra.it.validatesElement({ selector: '#test-aggregator' });
+    it('displays aggregator with disclosure - close from focus release', () => {
+      browser.waitForVisible('[class*="slide-group"] .close-disclosure');
+      browser.click('#test-aggregator #section1');
+      Terra.validates.element('close from focus release', { selector: '#test-aggregator' });
     });
 
-    describe('With Disclosure - Close from explicit close', () => {
-      before(() => {
-        browser.click('#test-aggregator #section1');
-
-        browser.waitForVisible('[class*="slide-group"] .close-disclosure');
-
-        browser.click('[class*="slide-group"] .close-disclosure');
-      });
-
-      Terra.it.validatesElement({ selector: '#test-aggregator' });
+    it('displays aggregator with disclosure - close from explicit close', () => {
+      browser.click('#test-aggregator #section1');
+      browser.waitForVisible('[class*="slide-group"] .close-disclosure');
+      browser.click('[class*="slide-group"] .close-disclosure');
+      Terra.validates.element('close from explicit close', { selector: '#test-aggregator' });
     });
 
-    describe('With Disclosure - Close from explicit button flip', () => {
-      before(() => {
-        browser.click('#test-aggregator #section1');
-        browser.waitForVisible('[class*="slide-group"] .close-disclosure');
-        browser.click('#flip-button');
-      });
-
-      Terra.it.validatesElement({ selector: '#test-aggregator' });
+    it('displays aggregator with disclosure - close from explicit button flip', () => {
+      browser.click('#test-aggregator #section1');
+      browser.waitForVisible('[class*="slide-group"] .close-disclosure');
+      browser.click('#flip-button');
+      Terra.validates.element('close from explicit button flip', { selector: '#test-aggregator' });
     });
   });
 });
