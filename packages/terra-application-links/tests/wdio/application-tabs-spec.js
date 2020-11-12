6e4ab6b265464f78ae986a781ec46665b969ca8f
saket2403 <saket.bajaj1998@gmail.com>
2020-11-12 11:48:28 +0530

    Comments

--- packages/terra-application-links/tests/wdio/application-tabs-spec.js
+++ packages/terra-application-links/tests/wdio/application-tabs-spec.js
@@ -1,6 +1,6 @@
 Terra.describeViewports('ApplicationTabs', ['small', 'large'], () => {
   it('Displays a default application tabs', () => {
-    browser.url('/#/raw/tests/terra-application-links/application-links/application-tabs-default');
+    browser.url('/raw/tests/terra-application-links/application-links/application-tabs-default');
     browser.waitForVisible('#test-tabs [data-application-tabs-more]');
     browser.moveToObject('#test-tabs [data-application-tabs-more]');
     Terra.validates.element();
@@ -8,7 +8,7 @@ Terra.describeViewports('ApplicationTabs', ['small', 'large'], () => {
 
   describe('Displays collapsed application tabs', () => {
     it('Displays collapsed application tabs', () => {
-      browser.url('/#/raw/tests/terra-application-links/application-links/application-tabs-default');
+      browser.url('/raw/tests/terra-application-links/application-links/application-tabs-default');
       browser.waitForVisible('#test-tabs [data-application-tabs-more]');
       browser.click('#test-tabs [data-application-tabs-more]');
       browser.waitForVisible('[data-application-tab-menu-content]');
@@ -25,7 +25,7 @@ Terra.describeViewports('ApplicationTabs', ['small', 'large'], () => {
 
   describe('Displays a hidden application tabs selection', () => {
     it('Displays a hidden application tabs selection', () => {
-      browser.url('/#/raw/tests/terra-application-links/application-links/application-tabs-default');
+      browser.url('/raw/tests/terra-application-links/application-links/application-tabs-default');
       browser.waitForVisible('#test-tabs [data-application-tabs-more]');
       browser.click('#test-tabs [data-application-tabs-more] span');
       browser.waitForVisible('[data-application-tab-menu-content]');
@@ -36,45 +36,42 @@ Terra.describeViewports('ApplicationTabs', ['small', 'large'], () => {
   });
 
   it('Displays a application tabs start aligned', () => {
-    browser.url('/#/raw/tests/terra-application-links/application-links/application-tabs-start');
+    browser.url('/raw/tests/terra-application-links/application-links/application-tabs-start');
     browser.pause(50);
     Terra.validates.element('start aligned');
   });
 
   it('Displays a application tabs center aligned', () => {
-    browser.url('/#/raw/tests/terra-application-links/application-links/application-tabs-center');
+    browser.url('/raw/tests/terra-application-links/application-links/application-tabs-center');
     browser.pause(50);
     Terra.validates.element('center aligned');
   });
 
   it('Displays a application tabs end aligned', () => {
-    browser.url('/#/raw/tests/terra-application-links/application-links/application-tabs-end');
+    browser.url('/raw/tests/terra-application-links/application-links/application-tabs-end');
     browser.pause(50);
     Terra.validates.element('end aligned');
   });
 
   describe('Navigates away from the site', () => {
-    beforeEach(() => {
-      browser.url('/#/raw/tests/terra-application-links/application-links/application-tabs-external');
+    it('successfully navigated away', () => {
+      browser.url('/raw/tests/terra-application-links/application-links/application-tabs-external');
       browser.pause(50);
       browser.click('#test-tabs #test-button');
       browser.pause(100);
-    });
-
-    it('successfully navigated away', () => {
       expect(browser.url().value).to.equal('https://engineering.cerner.com/terra-ui/home/terra-ui/index');
     });
   });
 
   it('Displays an application tabs with icons', () => {
-    browser.url('/#/raw/tests/terra-application-links/application-links/application-tabs-with-icons');
+    browser.url('/raw/tests/terra-application-links/application-links/application-tabs-with-icons');
     browser.waitForVisible('#test-tabs [data-application-tabs-more]');
     browser.moveToObject('#test-tabs [data-application-tabs-more]');
     Terra.validates.element('with icons');
   });
 
   it('Displays collapsed application tabs with icons', () => {
-    browser.url('/#/raw/tests/terra-application-links/application-links/application-tabs-with-icons');
+    browser.url('/raw/tests/terra-application-links/application-links/application-tabs-with-icons');
     browser.waitForVisible('#test-tabs [data-application-tabs-more]');
     browser.click('#test-tabs [data-application-tabs-more]');
     browser.waitForVisible('[data-application-tab-menu-content]');
