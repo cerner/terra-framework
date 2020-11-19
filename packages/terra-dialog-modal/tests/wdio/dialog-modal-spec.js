3cf18218a93fa6d8ead492258be25dce3077334d
saket2403 <saket.bajaj1998@gmail.com>
2020-11-19 16:26:00 +0530

    Remove it helpers

--- packages/terra-dialog-modal/tests/wdio/dialog-modal-spec.js
+++ packages/terra-dialog-modal/tests/wdio/dialog-modal-spec.js
@@ -6,7 +6,7 @@ Terra.describeViewports('Dialog Modal', ['tiny', 'medium', 'large'], () => {
       browser.url('/#/raw/tests/terra-dialog-modal/dialog-modal/default-dialog-modal');
       browser.click('#trigger-dialog-modal');
 
-      Terra.validates.element({ selector });
+      Terra.validates.element('default', { selector });
     });
   });
 
@@ -15,7 +15,7 @@ Terra.describeViewports('Dialog Modal', ['tiny', 'medium', 'large'], () => {
       browser.url('/#/raw/tests/terra-dialog-modal/dialog-modal/dialog-modal-with-long-text');
       browser.click('#trigger-dialog-modal');
 
-      Terra.validates.element({ selector });
+      Terra.validates.element('long text', { selector });
     });
   });
 
@@ -24,55 +24,48 @@ Terra.describeViewports('Dialog Modal', ['tiny', 'medium', 'large'], () => {
       browser.url('/#/raw/tests/terra-dialog-modal/dialog-modal/dialog-modal-with-custom-header-and-custom-footer');
       browser.click('#trigger-dialog-modal');
 
-      Terra.validates.element({ selector });
+      Terra.validates.element('custom header and footer', { selector });
     });
   });
 });
 
 describe('Dialog Modal', () => {
   describe('Within Modal Focus Handling', () => {
-    before(() => browser.url('/#/raw/tests/terra-dialog-modal/dialog-modal/default-dialog-modal'));
-
     it('focuses on the modal when opened', () => {
+      browser.url('/#/raw/tests/terra-dialog-modal/dialog-modal/default-dialog-modal');
       browser.click('#trigger-dialog-modal');
       expect(browser.hasFocus('[aria-modal="true"][role="dialog"]')).to.be.equal(true);
+      Terra.validates.element('modal is focused on open', { selector: '#root' });
     });
-
-    Terra.it.matchesScreenshot('modal is focused on open', { selector: '#root' });
 
     it('focuses on interactive elements within the modal', () => {
       browser.keys(['Tab']);
       expect(browser.hasFocus('[type="button"][aria-label="Close"]')).to.be.equal(true);
+      Terra.validates.element('modal button focused', { selector: '#root' });
     });
-
-    Terra.it.matchesScreenshot('modal button focused', { selector: '#root' });
 
     it('does not focus on interactive content within the app when the modal is open - tab cycle forward', () => {
       browser.keys(['Tab']);
+      Terra.validates.element('focused shifted outside the end of the modal', { selector: '#root' });
     });
-
-    Terra.it.matchesScreenshot('focused shifted outside the end of the modal', { selector: '#root' });
 
     it('shifts focus back onto interactive elements within the modal', () => {
       browser.keys(['Shift', 'Tab']);
       expect(browser.hasFocus('[type="button"][aria-label="Close"]')).to.be.equal(true);
+      Terra.validates.element('modal button focused again', { selector: '#root' });
     });
-
-    Terra.it.matchesScreenshot('modal button focused again', { selector: '#root' });
   });
 
   describe('Outside Modal Focus Handling', () => {
     it('does not focus on interactive content within the app when modal is open and focus is shifted backwards from the modal dialog', () => {
       browser.keys(['Shift', 'Tab']);
+      Terra.validates.element('focused shifted outside the beginning of the modal', { selector: '#root' });
     });
-
-    Terra.it.matchesScreenshot('focused shifted outside the beginning of the modal', { selector: '#root' });
 
     it('does not focus on interactive content within the app when modal is open and focus is shifted forwards from the modal dialog', () => {
       browser.keys(['Tab', 'Tab', 'Tab']);
+      Terra.validates.element('focused shifted outside the end of the modal', { selector: '#root' });
     });
-
-    Terra.it.matchesScreenshot('focused shifted outside the end of the modal', { selector: '#root' });
   });
 
   describe('Closing Select inside modal using escape', () => {
