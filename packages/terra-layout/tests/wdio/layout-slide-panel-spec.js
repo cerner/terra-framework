aad83809b33c26dc28d1fc09ae698b7a452fde92
Saket Bajaj <42207428+saket2403@users.noreply.github.com>
2020-12-15 20:30:29 +0530

    [terra-layout]Remove Terra.it helpers from wdio (#1341)
    * Test
    
    * Remove hash
    
    * Fix
    
    * Test build
    
    * wdio
    
    * fix build
    
    Co-authored-by: Matt Henkes <mjhenkes@gmail.com>

--- packages/terra-layout/tests/wdio/layout-slide-panel-spec.js
+++ packages/terra-layout/tests/wdio/layout-slide-panel-spec.js
@@ -1,25 +1,24 @@
 /* globals $ */
 Terra.describeViewports('Toggle the slide panel and hidden styles', ['medium'], () => {
-  before(() => {
-    browser.url('/#/raw/tests/terra-layout/layout/layout-slide-panel-example');
+  it('toggles the slide panel and hidden styles', () => {
+    browser.url('/raw/tests/terra-layout/layout/layout-slide-panel-example');
+    browser.refresh();
     browser.waitForExist('[class*=_panel_][aria-hidden="true"]');
+    Terra.validates.element('toggle slide panel', { selector: '#site' });
   });
-
-  Terra.it.matchesScreenshot({ selector: '#site' });
 });
 
 describe('Toggle the slide panel click', () => {
-  before(() => {
-    browser.url('/#/raw/tests/terra-layout/layout/layout-slide-panel-example');
+  it('toggles the panel with click', () => {
+    browser.url('/raw/tests/terra-layout/layout/layout-slide-panel-example');
     browser.click('#test-toggle-1');
     browser.waitForExist('[class*=_panel_][aria-hidden="false"]');
+    Terra.validates.element('click', { selector: '#site' });
   });
-
-  Terra.it.matchesScreenshot({ selector: '#site' });
 });
 
 describe('Toggle the slide panel double click', () => {
-  before(() => {
+  it('toggles the slide panel with double click', () => {
     const toggleBtn1 = $('#test-toggle-1');
     browser.pause(150);
     const toggleBtn2 = $('#test-toggle-2');
@@ -27,7 +26,6 @@ describe('Toggle the slide panel double click', () => {
     browser.click('#test-toggle-2');
     browser.waitForExist('[class*=_panel_][aria-hidden="true"]');
     expect(toggleBtn1.hasFocus()).to.be.equal(true);
+    Terra.validates.element('double click', { selector: '#site' });
   });
-
-  Terra.it.matchesScreenshot({ selector: '#site' });
 });
