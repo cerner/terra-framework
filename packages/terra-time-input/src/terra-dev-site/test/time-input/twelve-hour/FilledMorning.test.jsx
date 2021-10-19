db5e347b4cdef21d83dd4f6af5dc81cb5e32e9d3
Elliott Hoffman <elliott.hoffman@cerner.com>
2021-10-19 10:36:09 -0500

    questions as a header

--- packages/terra-time-input/src/terra-dev-site/test/time-input/twelve-hour/FilledMorning.test.jsx
+++ packages/terra-time-input/src/terra-dev-site/test/time-input/twelve-hour/FilledMorning.test.jsx
@@ -12,13 +12,12 @@ const TimeInputDefault = () => (
 
       <h2>Situation:</h2>
       <p>We are working to make an existing component retroactively more accessible. Of course, it is not ideal, but we are working with requirements that dictate the current approach. We are working with the Titanic, and we can't simply turn the accessible corner at one time. Instead, we must ease into the path. That said, we want to make this version as accessible as possible within the given constraints.</p>
-      <p>We are essentially working with a dual input for time (an input for hours and an input for minutes) that visually appears as one input. Each input functions independently and combines the ease auto-advance or backspace to quickly key time values. We would appreciate any feedback you can afford to help us provide the right amount of context for assistive technology users or any other disabled user. Specifically, we would like feedback on the time input itself but welcome any constructive criticism or advice to improve the group of controls.</p>
+      <p>We are essentially working with a dual input for time (an input for hours and an input for minutes) that visually appears as one input. Each input functions independently and combines the ease of auto-advance or backspace to quickly key time values. We would appreciate any feedback you can afford to help us provide the right amount of context for assistive technology users or any other disabled user. Specifically, we would like feedback on the time input itself but welcome any constructive criticism or advice to improve the group of controls.</p>
       <h2>Known issues:</h2>
       <p>The AM / PM radio buttons have issues: they do not provide the proper context, the keyboard interactions do not meet expected behaviors, and other issues. We will fix all radio button issues.</p>
 
+      <h2>Questions for the accessibility community:</h2>
       <ul>
-        Questions for the accessibility community:
-
         <li>What works well and what doesn't?</li>
         <li>Does the Time input provide enough programmatic context for screen readers, or are any parts too verbose?</li>
         <li>How do you solve pronunciation issues with AM / PM options?</li>
