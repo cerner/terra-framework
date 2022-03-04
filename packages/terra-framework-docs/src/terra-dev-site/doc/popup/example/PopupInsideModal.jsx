bad2bb3018f1307f039519395131fede0ebe02c9
lokesh-0813 <lokesh.0813@gmail.com>
2022-03-04 17:35:04 +0530

    pilot

---
+++ packages/terra-framework-docs/src/terra-dev-site/doc/popup/example/PopupInsideModal.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ModalManager from 'terra-modal-manager';
+import ModalContainer from './PopupModalContainer';
+
+function ModalManagerDemo() {
+  return (
+    <ModalManager>
+      <ModalContainer />
+    </ModalManager>
+  );
+}
+
+export default ModalManagerDemo;
