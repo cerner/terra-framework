72d0ff4e0dff3f87ca4255bb89c439292dbd532d
neilpfeiffer <neil.pfeiffer@cerner.com>
2023-02-27 08:04:56 -0600

    restructured for disabled menu items with custom icon color, removed incorrect focus glow, updated WDIO

--- packages/terra-framework-docs/src/terra-dev-site/test/menu/menu-item/MenuItemSubMenu.test.jsx
+++ packages/terra-framework-docs/src/terra-dev-site/test/menu/menu-item/MenuItemSubMenu.test.jsx
@@ -6,7 +6,7 @@ import styles from './MenuItemCommon.module.scss';
 const cx = classNames.bind(styles);
 
 const MenuItemSubMenu = () => (
-  <ul className={cx('unordered-list')} role="menu">
+  <ul className={cx('mimic-menu-container')} role="menu">
     <Menu.Item
       className="TestSubMenuItem"
       text="MenuItem - Submenu"
--- packages/terra-framework-docs/src/terra-dev-site/test/menu/menu-item/MenuItemsWithIcons.test.jsx
+++ packages/terra-framework-docs/src/terra-dev-site/test/menu/menu-item/MenuItemsWithIcons.test.jsx
@@ -4,6 +4,7 @@ import classNames from 'classnames/bind';
 import Menu from 'terra-menu';
 import IconAlert from 'terra-icon/lib/icon/IconAlert';
 import IconRequired from 'terra-icon/lib/icon/IconRequired';
+import ThemeContext from 'terra-theme-context';
 import styles from './MenuItemCommon.module.scss';
 
 const cx = classNames.bind(styles);
@@ -18,11 +19,12 @@ class MenuItemWithIcon extends React.Component {
   }
 
   render() {
+    const theme = this.context;
     return (
       <div>
         <p>default icon, themeable, isToggled, isToggleable, and isInstructionsForUse combinations</p>
         <br />
-        <ul className={cx('unordered-list')} role="menu">
+        <ul className={cx('mimic-menu-container')} role="menu">
           <Menu.Item
             text="Icon"
             key="Icon"
@@ -31,7 +33,7 @@ class MenuItemWithIcon extends React.Component {
           <Menu.Item
             text="Icon, themeable"
             key="Icon, themeable"
-            icon={<IconRequired className={cx('red-color')} />}
+            icon={<IconRequired className={cx(['red-color', theme.className])} />}
           />
           <Menu.Item
             text="Icon, isToggleable"
@@ -60,6 +62,13 @@ class MenuItemWithIcon extends React.Component {
             icon={<IconAlert />}
             isInstructionsForUse
           />
+          <Menu.Item
+            text="MenuItem - Submenu"
+            key="MenuItem - Submenu"
+            subMenuItems={[
+              <Menu.Item text="Not Visible" />,
+            ]}
+          />
         </ul>
       </div>
     );
@@ -67,4 +76,5 @@ class MenuItemWithIcon extends React.Component {
 }
 
 MenuItemWithIcon.childContextTypes = childContextTypes;
+MenuItemWithIcon.contextType = ThemeContext;
 export default MenuItemWithIcon;
