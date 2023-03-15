4927b231d25fb79fc166561593affd574c6a5912
Parkeenvincha,Art <Art.Parkeenvincha@cerner.com>
2023-03-15 07:35:06 -0700

    Added test for disabled button after opening slide panel

---
+++ packages/terra-framework-docs/src/terra-dev-site/test/slide-panel/SlidePanelDisabledToggle.test.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import classNames from 'classnames/bind';
+import SlidePanel from 'terra-slide-panel';
+import styles from './SlidePanelDocCommon.test.module.scss';
+
+const cx = classNames.bind(styles);
+
+class SlidePanelDemo extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { panelIsOpen: false, buttonDisabled: false };
+    this.handleOtherButton = this.handleOtherButton.bind(this);
+    this.handlePanelToggle = this.handlePanelToggle.bind(this);
+  }
+
+  handleOtherButton() {
+    console.log('Other button pressed');
+  }
+
+  handlePanelToggle() {
+    this.setState(prevState => ({ panelIsOpen: !prevState.panelIsOpen, buttonDisabled: true }));
+  }
+
+  render() {
+    return (
+      <div className={cx('content-wrapper-toggle')}>
+        <SlidePanel
+          id="test-slide"
+          mainContent={(
+            <div className={cx('main-content')}>
+              <button type="button" id="other-button" className={cx('button')} onClick={this.handleOtherButton}>Other Button</button>
+              <button type="button" id="test-toggle" className={cx('button')} onClick={this.handlePanelToggle} disabled={this.state.buttonDisabled}>toggle</button>
+              <button type="button" id="another-button" className={cx('button')} onClick={this.handleOtherButton}>Another Button</button>
+            </div>
+          )}
+          panelContent={(
+            <div id="panel-content" className={cx('panel-content')}>
+              <button type="button" id="other-button" className={cx('button')} onClick={this.handleOtherButton}>Other Button</button>
+              <button id="focus-button" type="button" className={cx('button')} onClick={this.handlePanelToggle}>Close panel</button>
+            </div>
+          )}
+          panelAriaLabel="Panel content area"
+          mainAriaLabel="Main content area"
+          panelSize="small"
+          panelBehavior="overlay"
+          isOpen={this.state.panelIsOpen}
+          fill
+        />
+      </div>
+    );
+  }
+}
+
+export default SlidePanelDemo;
