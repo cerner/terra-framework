e01672c83ae18f2bd1fa4194aa3d5906f86e12f3
SC069282 <sydneyycombs@gmail.com>
2023-03-16 11:01:47 -0500

    Fix lint

--- packages/terra-framework-docs/src/terra-dev-site/test/slide-panel/SlidePanelMultipleButtonsToggle.test.jsx
+++ packages/terra-framework-docs/src/terra-dev-site/test/slide-panel/SlidePanelMultipleButtonsToggle.test.jsx
@@ -10,11 +10,11 @@ class SlidePanelDemo extends React.Component {
     super(props);
 
     this.state = { panelIsOpen: false };
-    this.handleOtherButton = this.handleOtherButton.bind(this);
     this.handlePanelToggle = this.handlePanelToggle.bind(this);
   }
 
-  handleOtherButton() {
+  static handleOtherButton() {
+    // eslint-disable-next-line no-console
     console.log('Other button pressed');
   }
 
@@ -29,14 +29,14 @@ class SlidePanelDemo extends React.Component {
           id="test-slide"
           mainContent={(
             <div className={cx('main-content')}>
-              <button type="button" id="other-button" className={cx('button')} onClick={this.handleOtherButton}>Other Button</button>
+              <button type="button" id="other-button" className={cx('button')} onClick={SlidePanelDemo.handleOtherButton}>Other Button</button>
               <button type="button" id="test-toggle" className={cx('button')} onClick={this.handlePanelToggle}>toggle</button>
-              <button type="button" id="another-button" className={cx('button')} onClick={this.handleOtherButton}>Another Button</button>
+              <button type="button" id="another-button" className={cx('button')} onClick={SlidePanelDemo.handleOtherButton}>Another Button</button>
             </div>
           )}
           panelContent={(
             <div id="panel-content" className={cx('panel-content')}>
-              <button type="button" id="other-button" className={cx('button')} onClick={this.handleOtherButton}>Other Button</button>
+              <button type="button" id="other-button" className={cx('button')} onClick={SlidePanelDemo.handleOtherButton}>Other Button</button>
               <button id="focus-button" type="button" className={cx('button')} onClick={this.handlePanelToggle}>Close panel</button>
             </div>
           )}
