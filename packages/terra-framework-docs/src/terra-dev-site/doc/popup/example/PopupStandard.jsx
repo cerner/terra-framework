c2eb15c759d9d7a2db3dfc1bc1dbe0d8bf2fba21
SG067724 <SG067724@cerner.net>
2023-03-27 12:57:47 +0530

    Example Updates

--- packages/terra-framework-docs/src/terra-dev-site/doc/popup/example/PopupStandard.jsx
+++ packages/terra-framework-docs/src/terra-dev-site/doc/popup/example/PopupStandard.jsx
@@ -1,11 +1,15 @@
 import React, { useRef, useState } from 'react';
 import Button from 'terra-button';
 import Popup from 'terra-popup';
-import { Placeholder } from '@cerner/terra-docs';
+import classNames from 'classnames/bind';
+import styles from './PopupDocCommon.module.scss';
+
+const cx = classNames.bind(styles);
 
 function PopupStandard() {
   const buttonElement = useRef();
   const [open, setOpen] = useState(false);
+  const [showDetails, setShowDetails] = useState(false);
 
   const setButtonNode = (node) => {
     buttonElement.current = node;
@@ -21,15 +25,34 @@ function PopupStandard() {
     setOpen(false);
   };
 
+  const handlePopUpButton = () => {
+    setShowDetails(true);
+  };
+
   return (
     <React.Fragment>
-      <Button text="Default Popup" onClick={handleButtonClick} refCallback={setButtonNode} />
+      <Button text="Default Popup" onClick={handleButtonClick} refCallback={setButtonNode} title="This is a pop up" />
       <Popup
         isOpen={open}
         targetRef={getButtonNode}
         onRequestClose={handleRequestClose}
+        isHeaderDisabled={false}
+        isContentFocusDisabled
       >
-        <Placeholder title="Popup Content" />
+        <div>
+          Click on the button to view details
+        </div>
+        {showDetails ? (
+          <div>
+            <Button text="Hide Details" onClick={() => setShowDetails(false)} className={cx('popup-button')} />
+            <div>
+              This is the example of a default pop up.
+            </div>
+          </div>
+        )
+          : (
+            <Button text="Show Details" onClick={handlePopUpButton} className={cx('popup-button')} />
+          )}
       </Popup>
     </React.Fragment>
   );
