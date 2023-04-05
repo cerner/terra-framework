1f4bada4814123e29951ac7832a0ef63130c6fc5
st06365 <st063655@cerner.net>
2023-04-05 18:59:45 +0530

    Merge the changes.

--- packages/terra-menu/src/_MenuContent.jsx
+++ packages/terra-menu/src/_MenuContent.jsx
@@ -294,19 +294,7 @@ class MenuContent extends React.Component {
 
     if (this.props.index > 0) {
       header = (
-<<<<<<< HEAD
-        <div className={cx('header-container')}>
-          <div className={cx('header-button')}
-          role="button"
-          onClick={this.props.onRequestBack}
-          onKeyDown={this.onKeyDownBackButton}
-          tabIndex="0"
-          aria-label={backBtnText}
-          aria-description={this.props.title}
-        >
-=======
         <>
->>>>>>> Fix for Sub_Prop
           <div className={cx('header-container')}>
             <div
               className={cx('header-button')}
@@ -321,12 +309,7 @@ class MenuContent extends React.Component {
             </div>
             <h1 id={menuHeaderId} className={cx('header-title')}>{this.props.title}</h1>
           </div>
-<<<<<<< HEAD
-          </div>
-      </div>
-=======
         </>
->>>>>>> Fix for Sub_Prop
       );
     } else if (this.props.headerTitle.length > 0) {
       header = (
