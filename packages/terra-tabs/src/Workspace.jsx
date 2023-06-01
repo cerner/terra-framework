cc4a087ce069389587dc8c7b4c83940e0ae6d5bd
Sameena Taj <126573882+ST063655@users.noreply.github.com>
2023-06-01 10:47:15 +0530

    Example for Workspace Tab with Icon Custom Display. (#1663)
    Co-authored-by: st06365 <st063655@cerner.net>

---
+++ packages/terra-tabs/src/Workspace.jsx
@@ -0,0 +1,120 @@
+import React, { useRef, useState } from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+import classNamesBind from 'classnames/bind';
+import ThemeContext from 'terra-theme-context';
+
+import { useIntl } from 'react-intl';
+
+import usePortalManager, { getPortalElement } from './usePortalManager';
+
+import Tabs from './common tabs/_Tabs';
+
+import styles from './Workspace.module.scss';
+
+const cx = classNamesBind.bind(styles);
+
+const propTypes = {
+  /**
+   * The itemKey associated to the active WorkspaceItem.
+   */
+  activeItemKey: PropTypes.string.isRequired,
+  /**
+   * The size string value matching the active size option.
+   */
+  activeSize: PropTypes.string,
+  /**
+   * The child WorkspaceItems.
+   */
+  children: PropTypes.node.isRequired,
+  /**
+   * The unique identifier used for accessibility mappings.
+   */
+  id: PropTypes.string.isRequired,
+  /**
+   * The function callback triggering when a item is selected.
+   * Returns the associated itemKey and metaData. e.g. onRequestActivate(itemKey, metaData)
+   */
+  onRequestActivate: PropTypes.func.isRequired
+};
+
+const getTabId = (id, itemKey) => `${id}-${itemKey}`;
+
+const getAssociatedPanelId = (id, itemKey) => `${getTabId(id, itemKey)}-panel`;
+
+
+const Workspace = ({
+  id,
+  activeItemKey,
+  activeSize,
+  children,
+  onRequestActivate,
+  ...customProps
+}) => {
+  const theme = React.useContext(ThemeContext);
+
+  const [workspaceContainerRef, workspacePortalsRef] = usePortalManager(activeItemKey);
+
+  const ariaLabel = "workspace"
+
+  const tabData = React.Children.map(children, child => ({
+    id: getTabId(id, child.props.itemKey),
+    itemKey: child.props.itemKey,
+    associatedPanelId: getAssociatedPanelId(id, child.props.itemKey),
+    label: child.props.label,
+    icon: child.props.icon,
+    isIconOnly: child.props.isIconOnly,
+    customDisplay: child.props.customDisplay,
+    isSelected: child.props.itemKey === activeItemKey,
+    onSelect: onRequestActivate,
+    metaData: child.props.metaData,
+  }));
+
+  const containerClassNames = classNames(
+    cx(
+      'workspace-container',
+      theme.className,
+    )
+  );
+
+  return (
+      <div
+        className={cx('workspace')}
+        role="none"
+      >
+        <div aria-hidden className={cx('body-shadow-container')}>
+          <div className={cx('body-shadow')} />
+        </div>
+        <div role="none" className={cx('tab-header')}>
+          <Tabs ariaLabel={ariaLabel} tabData={tabData} />
+        </div>
+        <div role="none" className={cx('body')} ref={workspaceContainerRef}>
+          {React.Children.map(children, child => {
+            let portalElement = workspacePortalsRef.current[child.props.itemKey]?.element;
+            if (!portalElement) {
+              portalElement = getPortalElement();
+              portalElement.setAttribute('role', 'none');
+
+              workspacePortalsRef.current[child.props.itemKey] = {
+                element: portalElement,
+              };
+            }
+
+            return (
+              React.cloneElement(child, {
+                key: child.props.itemKey,
+                id: getTabId(id, child.props.itemKey),
+                associatedPanelId: getAssociatedPanelId(id, child.props.itemKey),
+                isActive: child.props.itemKey === activeItemKey,
+                portalElement,
+              })
+            );
+          })}
+        </div>
+      </div>
+  );
+};
+
+Workspace.propTypes = propTypes;
+
+export default Workspace;
