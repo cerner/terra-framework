ee8d98a001acabfeac65b6e8c29c7cb4ba542896
Charles McDonald <charles.mcdonald@cerner.com>
2023-06-06 20:11:26 -0500

    UXPLATFORM-9139: Add logic to allow cell content to be masked.

---
+++ packages/terra-framework-docs/src/terra-dev-site/test/worklist-data-grid/WorklistDataGridMaskedCell.test.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import WorklistDataGrid from 'terra-worklist-data-grid';
+import gridDataJSON from './gridMaskedCellData.json';
+
+const WorklistDataGridMaskedCell = () => {
+  const rowHeaderIndex = 0;
+  const { cols, rows } = gridDataJSON;
+
+  return (
+    <React.Fragment>
+      <WorklistDataGrid
+        id="default-terra-worklist-data-grid"
+        columns={cols}
+        rows={rows}
+        rowHeaderIndex={rowHeaderIndex}
+        rowHeight="50px"
+        columnWidth="50px"
+        columnHeaderHeight="50px"
+        ariaLabel="Worklist Data Grid"
+      />
+    </React.Fragment>
+  );
+};
+
+export default WorklistDataGridMaskedCell;
