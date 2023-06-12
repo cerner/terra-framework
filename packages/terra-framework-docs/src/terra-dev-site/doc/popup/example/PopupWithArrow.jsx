efacc7af454abdf098b426e82e3c0e6a446d786b
ashishkcerner <ashish.kumbhare@cerner.com>
2023-06-12 15:42:09 +0530

    Popup example issue fix

--- packages/terra-framework-docs/src/terra-dev-site/doc/popup/example/PopupWithArrow.jsx
+++ packages/terra-framework-docs/src/terra-dev-site/doc/popup/example/PopupWithArrow.jsx
@@ -5,6 +5,7 @@ import Popup from 'terra-popup';
 function PopupArrow() {
   const buttonElement = useRef();
   const [open, setOpen] = useState(false);
+  const [focusedIndex, setFocusedIndex] = useState(-1);
 
   const setButtonNode = (node) => {
     buttonElement.current = node;
@@ -18,43 +19,66 @@ function PopupArrow() {
 
   const handleRequestClose = () => {
     setOpen(false);
+    buttonElement.current.focus(); // Return focus to the button when the popup closes
+  };
+
+  const handleKeyDown = (event) => {
+    if (event.key === 'ArrowDown') {
+      event.preventDefault();
+      setFocusedIndex((prevIndex) => (prevIndex + 1) % 2);
+    } else if (event.key === 'ArrowUp') {
+      event.preventDefault();
+      setFocusedIndex((prevIndex) => (prevIndex - 1 + 2) % 2);
+    }
   };
 
   useEffect(() => {
     if (open) {
-      const firstLink = document.querySelector('.terra-popup-content a');
-      if (firstLink) {
-        firstLink.focus();
-      }
+      setFocusedIndex(0);
     }
   }, [open]);
 
+
+  useEffect(() => {
+    if (open && focusedIndex !== -1) {
+      const listItems = document.querySelectorAll('.terra-popup-content a');
+      console.log("CHEKING ",listItems)
+      if (listItems.length > 0) {
+        listItems[focusedIndex].focus();
+      }
+    }
+  }, [open, focusedIndex]);
+
   return (
     <React.Fragment>
-      <Button text="Medication Links" onClick={handleButtonClick} refCallback={setButtonNode} />
+      <Button text="Medication Links" onClick={handleButtonClick} refCallback={setButtonNode} aria-label="Medication Links" />
       <Popup
         isOpen={open}
         onRequestClose={handleRequestClose}
         targetRef={getButtonNode}
         isArrowDisplayed
+        tabIndex={-1} // Prevent the popup from receiving focus
       >
-        <div className="terra-popup-content">
-          <a
-            href="https://www.refseek.com/directory/health_medical.html"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Health and Medical sites
-          </a>
-          <br />
-          <a
-            href="https://www.happiesthealth.com/?utm_source=google&utm_medium=cpc-hp"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Health Related Links
-          </a>
-        </div>
+        <ul style={{listStyleType:'none',padding:0,margin:0}} className="terra-popup-content" onKeyDown={handleKeyDown} role="menu">
+          <li tabIndex={0} aria-selected={focusedIndex === 0} role="menuitem">
+            <a
+              href="https://www.refseek.com/directory/health_medical.html"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Health and Medical sites
+            </a>
+          </li>
+          <li tabIndex={0} aria-selected={focusedIndex === 1} role="menuitem">
+            <a
+              href="https://www.happiesthealth.com/?utm_source=google&utm_medium=cpc-hp"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Health Related Links
+            </a>
+          </li>
+        </ul>
       </Popup>
     </React.Fragment>
   );
