9ac225a18fa7392407fd9c4ca8ff9a16da39a980
Charles McDonald <charles.mcdonald@cerner.com>
2023-06-14 11:05:26 -0500

    Squashed commit of the following:
    commit 87c19d0dc8272602c7d8a72a9f0926ef5c96e02b
    Author: Charles McDonald <charles.mcdonald@cerner.com>
    Date:   Wed Jun 14 10:58:36 2023 -0500
    
        UXPLATFORM-9139: Add Jest tests
    
    commit f8c81277e765bdf7e4eb99e00d18b3c3425868f8
    Author: Charles McDonald <charles.mcdonald@cerner.com>
    Date:   Tue Jun 13 14:10:14 2023 -0500
    
        Squashed commit of the following:
    
        commit 62283d428d8e8a40d586c0baf07ad3411d616732
        Author: SC069282 <sydneyycombs@gmail.com>
        Date:   Tue Jun 13 12:08:35 2023 -0500
    
            Update WDIO snapshots
    
        commit f2a6a8922c1e15f40bca84fbbeb86c1c1db5f14b
        Author: Saad Adnan <38024451+sdadn@users.noreply.github.com>
        Date:   Mon Jun 12 11:41:08 2023 -0500
    
            Update WorklistDataGrid.module.scss
    
        commit ac87a3ae10b00c7a45b1ec1e4b9b925d992631f3
        Author: Saad <38024451+sdadn@users.noreply.github.com>
        Date:   Mon Jun 12 11:38:54 2023 -0500
    
            removed css changes
    
        commit ecf8820d631c828e4d3812f4a2a4f2c18c728516
        Author: Saad <38024451+sdadn@users.noreply.github.com>
        Date:   Mon Jun 12 09:25:26 2023 -0500
    
            added default values
    
        commit 60e54bd1dcee718f773a6e02de4584f4c78e53e8
        Author: Saad Adnan <38024451+sdadn@users.noreply.github.com>
        Date:   Fri Jun 9 16:51:25 2023 -0500
    
            Update WorklistDataGrid.test.jsx
    
        commit 124289f28d1144b49f0da51b29406bb5f5199107
        Author: SC069282 <sydneyycombs@gmail.com>
        Date:   Fri Jun 9 16:36:02 2023 -0500
    
            Fix lint
    
        commit fb8d0acddffecb5fe62e25d17895e14ee7599633
        Author: SC069282 <sydneyycombs@gmail.com>
        Date:   Fri Jun 9 16:34:24 2023 -0500
    
            Add Jest tests
    
        commit c1c75e5f506ff8ba38e3a2295f8c87546b6c637b
        Author: SC069282 <sydneyycombs@gmail.com>
        Date:   Fri Jun 9 15:03:24 2023 -0500
    
            initial commit
    
    commit 30177583a0af0b83cb377e95a2a43462a02942a3
    Author: Koushik Kalli <Koushik.Kalli@Cerner.com>
    Date:   Mon Jun 12 21:44:57 2023 +0000
    
        Reordered css attributes to resolve linter error
    
    commit a214d628f6fe8fcae9016de63c9ce04a5dea500a
    Merge: c253d06a5 90781b70d
    Author: Koushik Kalli <126616925+KoushikKalli@users.noreply.github.com>
    Date:   Mon Jun 12 15:06:29 2023 -0400
    
        Merge branch 'main' into UXPLATFORM-9139
    
    commit c253d06a55f8cfd9447be3078295f5cecca8da5c
    Author: Koushik Kalli <Koushik.Kalli@Cerner.com>
    Date:   Mon Jun 12 19:03:06 2023 +0000
    
        Changed reference screenshots to include cell padding
    
    commit 90781b70d6f5f8d98ed5dac12668cf7464228dfa
    Author: cm9361 <59840481+cm9361@users.noreply.github.com>
    Date:   Mon Jun 12 13:43:15 2023 -0500
    
        [terra-worklist-data-grid] Remove ability to vary the row height (#1670)
    
    commit 57af9e37bd5ae874354bf90f9a16256f46a60d59
    Author: Charles McDonald <charles.mcdonald@cerner.com>
    Date:   Mon Jun 12 13:37:58 2023 -0500
    
        UXPLATFORM-9139: Add padding for cell contents.
    
    commit f6e14b1fa2b482862a95e49e2b0d50a05b9de303
    Author: Alla Doroshkevych <alla.doroshkevych@oracle.com>
    Date:   Mon Jun 12 12:03:39 2023 -0400
    
        release June 12 (#1675)
    
    commit 1e9fe5bf822adcff34307d52c9d0c8ff32d7890f
    Author: Alla Doroshkevych <alla.doroshkevych@oracle.com>
    Date:   Fri Jun 9 15:54:03 2023 -0400
    
        uuid locked to v8.2.0 (#1672)
    
    commit acfaff6af8239744cf9342888f7267ee29471513
    Author: Sydney Combs <42594047+sycombs@users.noreply.github.com>
    Date:   Tue Jun 6 15:41:42 2023 -0500
    
        Prepare June 5 release (#1668)
    
    commit d71b385ec560950da677e0ec0c219c182b9aaa1a
    Author: Sydney Combs <42594047+sycombs@users.noreply.github.com>
    Date:   Mon Jun 5 15:37:07 2023 -0500
    
        Update uuid versions (#1664)

--- packages/terra-worklist-data-grid/tests/jest/WorklistDataGrid.test.jsx
+++ packages/terra-worklist-data-grid/tests/jest/WorklistDataGrid.test.jsx
@@ -1,35 +1,40 @@
 // eslint-disable no-console
 import React from 'react';
 /* eslint-disable-next-line import/no-extraneous-dependencies */
+import { shallowWithIntl } from 'terra-enzyme-intl';
 import WorklistDataGrid from '../../src/WorklistDataGrid';
 import dataFile from './testData.json';
 
 describe('WorklistDataGrid', () => {
   it('renders a grid with 3 columns and 3 data rows', () => {
-    const wrapper = shallow(
+    const wrapper = shallowWithIntl(
       <WorklistDataGrid
         id="test-terra-worklist-data-grid"
         columns={dataFile.cols}
         rows={dataFile.rows}
       />,
-    );
+    ).dive();
 
     const columnHeaders = wrapper.find('.worklist-data-grid-column-header');
-    const rowHeaders = wrapper.find('.worklist-data-grid-row-header');
+    const rowHeaders = wrapper.find('.worklist-data-grid-cell');
+
+    // Test masked cells
+    const maskedCells = wrapper.find('.masked');
 
     expect(columnHeaders).toHaveLength(3);
-    expect(rowHeaders).toHaveLength(3);
+    expect(rowHeaders).toHaveLength(9);
+    expect(maskedCells).toHaveLength(3);
     expect(wrapper).toMatchSnapshot();
   });
 
   it('sets the key prop for columns', () => {
-    const wrapper = shallow(
+    const wrapper = shallowWithIntl(
       <WorklistDataGrid
         id="test-terra-worklist-data-grid"
         columns={dataFile.cols}
         rows={dataFile.rows}
       />,
-    );
+    ).dive();
 
     const column = wrapper.find('.worklist-data-grid-column-header').first();
     expect(column.key()).toEqual('Column-0');
@@ -39,7 +44,7 @@ describe('WorklistDataGrid', () => {
     jest.spyOn(console, 'error').mockImplementation();
 
     // eslint-disable-next-line no-unused-vars
-    const wrapper = (<WorklistDataGrid />);
+    const wrapper = shallowWithIntl(<WorklistDataGrid />);
     expect(console.error).toHaveBeenCalledWith(expect.stringContaining('The prop `id` is marked as required'));
 
     console.error.mockRestore();
