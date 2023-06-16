df48e9007a678b833221b55b42e002969d004753
Charles McDonald <charles.mcdonald@cerner.com>
2023-06-16 17:34:46 -0500

    Resolve UX code review comments

--- packages/terra-worklist-data-grid/src/proptypes/cellShape.js
+++ packages/terra-worklist-data-grid/src/proptypes/cellShape.js
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types';
 
 const cellShape = PropTypes.shape({
   /**
-   * String indicating content to be displayed for the cell.
+   * String indicating content to be displayed for the cell.  Content will be ignored if isMasked property is true.
    */
   content: PropTypes.string,
 
