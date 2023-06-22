1abe0b02ce4865c4b7be9f421d65fcd2cd1f6661
Sugan G <Sugan.G@cerner.com>
2023-06-22 10:19:42 +0530

    updated import

--- packages/terra-tabs/src/common-tabs/scroll-persistence.js
+++ packages/terra-tabs/src/common-tabs/scroll-persistence.js
@@ -1,4 +1,4 @@
-import uuidv4 from 'uuid/v4';
+import { v4 as uuidv4 } from 'uuid';
 
 function getOverflowDataForElement(overflowElement) {
   let overflowId = overflowElement.getAttribute('data-persistent-overflow-id');
