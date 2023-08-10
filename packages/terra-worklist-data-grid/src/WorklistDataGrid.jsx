eb34161fd074666dc473c7ebc101352459695977
Charles McDonald <charles.mcdonald@cerner.com>
2023-08-10 07:35:24 -0500

    Improve focus selector and update test snapshots.

--- packages/terra-worklist-data-grid/src/WorklistDataGrid.jsx
+++ packages/terra-worklist-data-grid/src/WorklistDataGrid.jsx
@@ -388,7 +388,7 @@ function WorklistDataGrid(props) {
    */
   const moveFocusFromGrid = (moveForward) => {
     // add all elements we want to include in our selection
-    const focusableElementSelector = `#${id}, a[href]:not([tabindex='-1']), area[href]:not([tabindex='-1']), input:not([disabled]):not([tabindex='-1']), `
+    const focusableElementSelector = 'a[href]:not([tabindex=\'-1\']), area[href]:not([tabindex=\'-1\']), input:not([disabled]):not([tabindex=\'-1\']), '
     + "select:not([disabled]):not([tabindex='-1']), textarea:not([disabled]):not([tabindex='-1']), button:not([disabled]):not([tabindex='-1']), "
     + "iframe:not([tabindex='-1']), [tabindex]:not([tabindex='-1']), [contentEditable=true]:not([tabindex='-1'])";
 
