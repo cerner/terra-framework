a2c38a2e7ce893bbf209b287cdcd3d1873858bde
ns065186 <ns065186@cerner.net>
2023-09-12 08:38:12 -0500

    Update label name prop as required

--- packages/terra-framework-docs/src/terra-dev-site/test/slider/DefaultSlider.test.jsx
+++ packages/terra-framework-docs/src/terra-dev-site/test/slider/DefaultSlider.test.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Slider from 'terra-slider';
 
 const DefaultSlider = () => (
-  <Slider minimumValue={0} maximumValue={100} />
+  <Slider minimumValue={0} maximumValue={100} labelName="Label" />
 );
 
 export default DefaultSlider;
