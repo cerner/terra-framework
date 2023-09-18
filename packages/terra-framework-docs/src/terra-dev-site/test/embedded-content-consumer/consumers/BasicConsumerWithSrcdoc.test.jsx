80335b383b9d0b207574d41bb0f74fba20403f7b
Kol Kheang <kol.kheang@oracle.com>
2023-09-18 11:21:10 -0400

    Update wdio test and snapshots

--- packages/terra-framework-docs/src/terra-dev-site/test/embedded-content-consumer/consumers/BasicConsumerWithSrcdoc.test.jsx
+++ packages/terra-framework-docs/src/terra-dev-site/test/embedded-content-consumer/consumers/BasicConsumerWithSrcdoc.test.jsx
@@ -11,11 +11,11 @@ Consumer.init();
 const BasicConsumerWithSrcDoc = () => (
   <EmbeddedContentConsumer
     className={cx('iframe')}
-    srcdoc="<h2>Inline HTML with srcdoc</h2><p>Inline HTML content can be used to render the view in the iframe.</p>"
     options={{
       iframeAttrs: {
         id: 'iframe-with-srcdoc', // unique id of this iframe
         title: 'Inline HTML Content', // title of the content inside the iframe
+        srcdoc: "<h2>Inline HTML with srcdoc</h2><p>Inline HTML content can be used to render the view in the iframe.</p>",
         width: '100%', // fixed width of this iframe (no auto resize logic)
         height: '100px', // fixed height of this iframe (no auto resize logic)
       },
