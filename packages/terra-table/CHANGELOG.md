160498f92cbf26d32eb030c7b2c622ece789d221
Charles McDonald <59840481+cm9361@users.noreply.github.com>
2023-11-08 09:26:42 -0600

    [terra-data-grid] Resize handle communication fix (#1878)

--- packages/terra-table/CHANGELOG.md
+++ packages/terra-table/CHANGELOG.md
@@ -10,6 +10,7 @@
   * Fixed the column header background color for the Orion Fusion theme.
   * Fixed issue where column header cell was receiving tab focus instead of the button for table role.
   * Fixed column header left border style.
+  * Fixed resize handle being communicated when non-header cell is focused.
   
 ## 5.1.1-alpha.0 - (October 25, 2023)
 
--- packages/terra-table/src/subcomponents/ColumnResizeHandle.jsx
+++ packages/terra-table/src/subcomponents/ColumnResizeHandle.jsx
@@ -109,8 +109,6 @@ const ColumnResizeHandle = (props) => {
   // Ref variable for native resize handle element
   const resizeHandleRef = useRef();
 
-  const [isAriaLabel, setIsAriaLabel] = useState(false);
-  const [isAriaValueText, setIsAriaValueText] = useState(false);
   const [isNavigationEnabled, setNavigationEnabled] = useState(true);
 
   useEffect(() => {
@@ -138,9 +136,6 @@ const ColumnResizeHandle = (props) => {
   const fitToTable = () => {
     // Update resize handle height to match parent table height
     resizeHandleRef.current.style.height = `${height}px`;
-
-    // Assistive technologies should announce aria-label text once focused
-    setIsAriaLabel(true);
   };
 
   const onMouseLeave = () => {
@@ -159,17 +154,12 @@ const ColumnResizeHandle = (props) => {
         // Lock focus into component
         resizeHandleRef.current.focus();
 
-        // Assistive technologies should avoid announcing aria-label while focus locked, but announce aria-valueText instead
-        setIsAriaLabel(false);
-        setIsAriaValueText(true);
         // Assistive technologies should make announcement for focus locked
         columnContext.setColumnHeaderAriaLiveMessage(intl.formatMessage({ id: 'Terra.table.cell-focus-trapped' }));
         event.stopPropagation();
         event.preventDefault();
         break;
       case KeyCode.KEY_ESCAPE:
-        // Assistive technologies should stop announcing aria-valueText once focus unlocked
-        setIsAriaValueText(false);
         // Release focus lock
         columnContext.setColumnHeaderAriaLiveMessage(intl.formatMessage({ id: 'Terra.table.resume-navigation' }));
         setNavigationEnabled(true);
@@ -203,7 +193,6 @@ const ColumnResizeHandle = (props) => {
 
   const onBlur = () => {
     setNavigationEnabled(true);
-    setIsAriaLabel(false);
     setIsActive(false);
   };
 
@@ -218,8 +207,8 @@ const ColumnResizeHandle = (props) => {
       aria-valuemin={isActive ? minimumWidth : null}
       aria-valuenow={isActive ? columnWidth : null}
       aria-valuemax={isActive ? maximumWidth : null}
-      aria-label={isAriaLabel ? intl.formatMessage({ id: 'Terra.table.resize-handle-template' }, { columnText }) : null}
-      aria-valuetext={isAriaValueText ? intl.formatMessage({ id: 'Terra.table.resize-handle-value-text' }, { columnWidth }) : null}
+      aria-label={isActive && isNavigationEnabled ? intl.formatMessage({ id: 'Terra.table.resize-handle-template' }, { columnText }) : null}
+      aria-valuetext={!isNavigationEnabled ? intl.formatMessage({ id: 'Terra.table.resize-handle-value-text' }, { columnWidth }) : null}
       style={{ height: `${height}px` }} // eslint-disable-line react/forbid-dom-props
       onMouseDown={onMouseDown}
       onMouseUp={onMouseUp}
