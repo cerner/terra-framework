a60c1c52389d252309f3aeaed4b1de21ace9f470
Alla Doroshkevych <alla.doroshkevych@oracle.com>
2023-11-20 01:35:43 -0500

    rowData moved to examples, a11yLabel added to icons

--- packages/terra-framework-docs/src/terra-dev-site/doc/compact-interactive-list/Examples/HorizontalFlow.jsx
+++ packages/terra-framework-docs/src/terra-dev-site/doc/compact-interactive-list/Examples/HorizontalFlow.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import CompactInteractiveList, { alignTypes } from 'terra-compact-interactive-list';
 import { ActiveBreakpointContext } from 'terra-breakpoints';
-import rows from './rowsData';
+import { IconDocuments, IconFeaturedOutline, IconImage } from 'terra-icon';
 
 const getNumberOfColumns = (activeBreakpoint) => {
   switch (activeBreakpoint) {
@@ -14,6 +14,49 @@ const getNumberOfColumns = (activeBreakpoint) => {
     default: return 1;
   }
 };
+
+const rows = [
+  {
+    id: 'row_1',
+    cells: [
+      { content: <IconDocuments a11yLabel="Documents" height="1.5em" width="1.5em" /> },
+      { content: 'Discern Care Set (1)' },
+      { content: <IconFeaturedOutline a11yLabel="Featured" height="1.5em" width="1.5em" /> },
+    ],
+  },
+  {
+    id: 'row_2',
+    cells: [
+      { content: <IconImage a11yLabel="Picture" height="1.5em" width="1.5em" /> },
+      { content: 'Initial observation Care/Day High Severity 99220 (2)' },
+      { content: <IconFeaturedOutline a11yLabel="Featured" height="1.5em" width="1.5em" /> },
+    ],
+  },
+  {
+    id: 'row_3',
+    cells: [
+      { content: <IconImage a11yLabel="Picture" height="1.5em" width="1.5em" /> },
+      { content: 'Arterial Sheath Care (3)' },
+      { content: <IconFeaturedOutline a11yLabel="Featured" height="1.5em" width="1.5em" /> },
+    ],
+  },
+  {
+    id: 'row_4',
+    cells: [
+      { content: ' ' },
+      { content: 'Sbsq Observation Care/Day High Severity 99226 (4)' },
+      { content: ' ' },
+    ],
+  },
+  {
+    id: 'row_5',
+    cells: [
+      { content: <IconImage a11yLabel="Picture" height="1.5em" width="1.5em" /> },
+      { content: 'Arterial Sheath Care (5)' },
+      { content: <IconFeaturedOutline a11yLabel="Featured" height="1.5em" width="1.5em" /> },
+    ],
+  },
+];
 
 const cols = [
   {
--- packages/terra-framework-docs/src/terra-dev-site/test/compact-interactive-list/ScalableUnits.test.jsx
+++ packages/terra-framework-docs/src/terra-dev-site/test/compact-interactive-list/ScalableUnits.test.jsx
@@ -1,7 +1,50 @@
 /* eslint-disable react/forbid-dom-props */
 import React from 'react';
 import CompactInteractiveList, { alignTypes } from 'terra-compact-interactive-list';
-import rows from './rowsData';
+import { IconDocuments, IconFeaturedOutline, IconImage } from 'terra-icon';
+
+const rows = [
+  {
+    id: 'row_1',
+    cells: [
+      { content: <IconDocuments a11yLabel="Documents" height="1.5em" width="1.5em" /> },
+      { content: 'Discern Care Set (1)' },
+      { content: <IconFeaturedOutline a11yLabel="Featured" height="1.5em" width="1.5em" /> },
+    ],
+  },
+  {
+    id: 'row_2',
+    cells: [
+      { content: <IconImage a11yLabel="Picture" height="1.5em" width="1.5em" /> },
+      { content: 'Initial observation Care/Day High Severity 99220 (2)' },
+      { content: <IconFeaturedOutline a11yLabel="Featured" height="1.5em" width="1.5em" /> },
+    ],
+  },
+  {
+    id: 'row_3',
+    cells: [
+      { content: <IconImage a11yLabel="Picture" height="1.5em" width="1.5em" /> },
+      { content: 'Arterial Sheath Care (3)' },
+      { content: <IconFeaturedOutline a11yLabel="Featured" height="1.5em" width="1.5em" /> },
+    ],
+  },
+  {
+    id: 'row_4',
+    cells: [
+      { content: ' ' },
+      { content: 'Sbsq Observation Care/Day High Severity 99226 (4)' },
+      { content: ' ' },
+    ],
+  },
+  {
+    id: 'row_5',
+    cells: [
+      { content: <IconImage a11yLabel="Picture" height="1.5em" width="1.5em" /> },
+      { content: 'Arterial Sheath Care (5)' },
+      { content: <IconFeaturedOutline a11yLabel="Featured" height="1.5em" width="1.5em" /> },
+    ],
+  },
+];
 
 const cols = [
   {
