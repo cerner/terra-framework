bbc0556d3a81073a3d5978ba2132a918a2d4db8a
Jessie Randle <108024283+JessieRandle@users.noreply.github.com>
2023-11-21 17:07:10 -0600

    Update packages/terra-folder-tree/src/subcomponents/FolderTreeItem.jsx
    Co-authored-by: Eric Wilson <eawww0@gmail.com>

--- packages/terra-folder-tree/src/subcomponents/FolderTreeItem.jsx
+++ packages/terra-folder-tree/src/subcomponents/FolderTreeItem.jsx
@@ -22,7 +22,7 @@ const propTypes = {
    */
   icon: PropTypes.element,
   /**
-   * List of FolderTree.Items to display in a subfolder when this FolderTreeItem is clicked.
+   * List of FolderTree.Items to display in a subfolder when this FolderTreeItem is clicked. This makes the item expandable and collapsible. It also makes the item's icon a Folder, overriding anything given as the icon prop.
    */
   subfolderItems: PropTypes.arrayOf(PropTypes.element),
   /**
