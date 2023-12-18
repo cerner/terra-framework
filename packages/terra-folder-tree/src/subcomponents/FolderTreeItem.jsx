a0940a1a372fac3d31f58e498193fb15661b4325
artpark <art.parkeenvincha@gmail.com>
2023-12-18 13:08:46 -0800

    Remove disabled eslint rule

--- packages/terra-folder-tree/src/subcomponents/FolderTreeItem.jsx
+++ packages/terra-folder-tree/src/subcomponents/FolderTreeItem.jsx
@@ -167,8 +167,6 @@ const FolderTreeItem = ({
 
   return (
     <>
-      {/* TODO: Re-enable this eslint rule once keyboard handling is implemented */}
-      {/* eslint-disable-next-line jsx-a11y/click-events-have-key-events */}
       <li
         className={itemClassNames}
         role="treeitem"
