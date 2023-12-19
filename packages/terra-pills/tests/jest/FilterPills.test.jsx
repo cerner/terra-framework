353ad876dad52654a4fc2a6f6cb2642e81f07d4d
SM051274 <sm051274@cerner.net>
2023-12-20 00:56:53 +0530

    test case update

---
+++ packages/terra-pills/tests/jest/FilterPills.test.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { shallowWithIntl } from 'terra-enzyme-intl';
+import { v4 as uuidv4 } from 'uuid';
+import FilterPills from '../../src/FilterPills';
+import Pill from '../../src/subcomponents/_Pill';
+
+describe('Rollup Pill', () => {
+  let mockSpyUuid;
+  beforeAll(() => {
+    mockSpyUuid = jest.spyOn(uuidv4, 'v4').mockReturnValue('00000000-0000-0000-0000-000000000000');
+  });
+
+  afterAll(() => {
+    mockSpyUuid.mockRestore();
+  });
+
+  it('should render pill with the role list when rendered with pills', () => {
+    const wrapper = shallowWithIntl(
+      <FilterPills
+        ariaLabel="Filter Pills with labels"
+      >
+        <Pill
+          label="asthma"
+          labelCategory="Allergies"
+          id="1234"
+          key="1234"
+        />
+      </FilterPills>,
+    ).dive();
+    const groupDiv = wrapper.find('div.pills-group');
+    expect(groupDiv.prop('role')).toEqual('list');
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('should render pill with the role group when rendered empty', () => {
+    const wrapper = shallowWithIntl(<FilterPills
+      ariaLabel="Empty Filter Pills"
+    />).dive();
+    const groupDiv = wrapper.find('div.pills-group');
+    expect(groupDiv.prop('role')).toEqual('group');
+    expect(wrapper).toMatchSnapshot();
+  });
+});
