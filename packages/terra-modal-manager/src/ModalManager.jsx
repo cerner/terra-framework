4ddf393c764067369ca4da45fe926edd17f2d3ec
AH106586Harika <AH106586Harika@cerner.com>
2024-02-12 15:03:14 +0530

    snap update

--- packages/terra-modal-manager/src/ModalManager.jsx
+++ packages/terra-modal-manager/src/ModalManager.jsx
@@ -37,7 +37,7 @@ const propTypes = {
   /**
    * If set to true, then the outside click will get enabled.
    */
-  closeOnOutsideClick: PropTypes.bool
+  closeOnOutsideClick: PropTypes.bool,
 };
 
 const defaultProps = {
