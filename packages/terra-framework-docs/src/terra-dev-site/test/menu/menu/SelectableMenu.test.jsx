8d85dc23e5028bfa79db7f85c7b9416cf85c5a62
ns065186 <ns065186@cerner.net>
2024-02-26 09:27:11 -0600

    Update variable name

--- packages/terra-framework-docs/src/terra-dev-site/test/menu/menu/SelectableMenu.test.jsx
+++ packages/terra-framework-docs/src/terra-dev-site/test/menu/menu/SelectableMenu.test.jsx
@@ -11,7 +11,7 @@ class SelectableMenu extends React.Component {
     this.handleRequestClose = this.handleRequestClose.bind(this);
     this.setButtonNode = this.setButtonNode.bind(this);
     this.getButtonNode = this.getButtonNode.bind(this);
-    this.state = { open: false, selectedGroupIndex: undefined };
+    this.state = { open: false, selectedGroupItemIndex: undefined };
   }
 
   handleButtonClick() {
@@ -19,7 +19,7 @@ class SelectableMenu extends React.Component {
   }
 
   handleOnChange(event, index) {
-    this.setState({ selectedGroupIndex: index });
+    this.setState({ selectedGroupItemIndex: index });
   }
 
   handleRequestClose() {
@@ -46,9 +46,9 @@ class SelectableMenu extends React.Component {
           onRequestClose={this.handleRequestClose}
         >
           <Menu.ItemGroup className="TestGroup" onChange={this.handleOnChange} key="selectable-group">
-            <Menu.Item text="Group Item 1" key="1" className="TestGroupItem1" isToggled={this.state.selectedGroupIndex === 0} />
-            <Menu.Item text="Group Item 2" key="2" className="TestGroupItem2" isToggled={this.state.selectedGroupIndex === 1} />
-            <Menu.Item text="Group Item 3" key="3" className="TestGroupItem3" isToggled={this.state.selectedGroupIndex === 2} />
+            <Menu.Item text="Group Item 1" key="1" className="TestGroupItem1" isToggled={this.state.selectedGroupItemIndex === 0} />
+            <Menu.Item text="Group Item 2" key="2" className="TestGroupItem2" isToggled={this.state.selectedGroupItemIndex === 1} />
+            <Menu.Item text="Group Item 3" key="3" className="TestGroupItem3" isToggled={this.state.selectedGroupItemIndex === 2} />
           </Menu.ItemGroup>
         </Menu>
         <Button id="selectable-menu-button" type="button" onClick={this.handleButtonClick} text="Default Menu" aria-haspopup icon={<IconCaretDown />} isReversed refCallback={this.setButtonNode} />
