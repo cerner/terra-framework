f4cdafb58a56ec11189ef2fa49e18a02e0429258
MadanKumarGovindaswamy <madankumar.govindaswamy@cerner.com>
2024-03-05 17:03:55 +0530

    Update NavigationSideMenuExample.jsx

--- packages/terra-framework-docs/src/terra-dev-site/doc/navigation-side-menu/example/NavigationSideMenuExample.jsx
+++ packages/terra-framework-docs/src/terra-dev-site/doc/navigation-side-menu/example/NavigationSideMenuExample.jsx
@@ -21,27 +21,27 @@ class NavigationSideMenuDefault extends React.Component {
 
   render() {
     let content;
-      content = (
-        <NavigationSideMenu
-          id="test-menu"
-          menuItems={[
-            { key: 'menu', text: 'Hospital Details', childKeys: ['submenu1', 'submenu2', 'submenu3', 'submenu4'] },
-            {
-              key: 'submenu1', text: 'Hospital services', childKeys: ['subsubmenu1', 'subsubmenu2', 'subsubmenu3'], id: 'test-item-1',
-            },
-            { key: 'submenu2', text: 'Hospital events' },
-            { key: 'submenu3', text: 'Hospital Accommodations' },
-            { key: 'submenu4', text: 'Hospital Careers' },
-            { key: 'subsubmenu1', text: 'Imaging', id: 'test-item-2' },
-            { key: 'subsubmenu2', text: 'Laboratory' },
-            { key: 'subsubmenu3', text: 'Rehabilitation services' },
-          ]}
-          onChange={this.handleOnChange}
-          selectedMenuKey={this.state.selectedMenuKey}
-          selectedChildKey={this.state.selectedChildKey}
-          ariaLabel="Sub Menu List"
-        />
-      );
+    content = (
+      <NavigationSideMenu
+        id="test-menu"
+        menuItems={[
+          { key: 'menu', text: 'Hospital Details', childKeys: ['submenu1', 'submenu2', 'submenu3', 'submenu4'] },
+          {
+            key: 'submenu1', text: 'Hospital services', childKeys: ['subsubmenu1', 'subsubmenu2', 'subsubmenu3'], id: 'test-item-1',
+          },
+          { key: 'submenu2', text: 'Hospital events' },
+          { key: 'submenu3', text: 'Hospital Accommodations' },
+          { key: 'submenu4', text: 'Hospital Careers' },
+          { key: 'subsubmenu1', text: 'Imaging', id: 'test-item-2' },
+          { key: 'subsubmenu2', text: 'Laboratory' },
+          { key: 'subsubmenu3', text: 'Rehabilitation services' },
+        ]}
+        onChange={this.handleOnChange}
+        selectedMenuKey={this.state.selectedMenuKey}
+        selectedChildKey={this.state.selectedChildKey}
+        ariaLabel="Sub Menu List"
+      />
+    );
 
     return (
       <div className={cx('content-wrapper')}>
