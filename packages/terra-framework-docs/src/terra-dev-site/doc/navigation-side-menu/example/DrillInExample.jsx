3553442eacbadab838d37e72a3c492fd337061c5
MadanKumarGovindaswamy <madankumar.govindaswamy@cerner.com>
2024-05-17 13:32:10 +0530

    lint error

--- packages/terra-framework-docs/src/terra-dev-site/doc/navigation-side-menu/example/DrillInExample.jsx
+++ packages/terra-framework-docs/src/terra-dev-site/doc/navigation-side-menu/example/DrillInExample.jsx
@@ -45,7 +45,7 @@ class DrillInDefault extends React.Component {
           },
           { key: 'subsubmenu2', text: 'Laboratory' },
           {
-            key: 'subsubmenu3', text: 'Rehabilitation services', childKeys: ['rehab1', 'rehab2', 'rehab3'], icon: <IconHospital a11yLabel='Location' />,
+            key: 'subsubmenu3', text: 'Rehabilitation services', childKeys: ['rehab1', 'rehab2', 'rehab3'], icon: <IconHospital a11yLabel="Location" />,
           },
           { key: 'rehab1', text: 'Rehabilitation services 1' },
           { key: 'rehab2', text: 'Rehabilitation services 2' },
